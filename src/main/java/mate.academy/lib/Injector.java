3a56b77312d88b4c6470cd192f3be9aaafde47c1
fotomassimo <maksym.pavliuchenko@gmail.com>
2021-05-04 21:35:46 +0300

    initial commit

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,9 +1,73 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
+    }
+
+    public Object getInstance(Class<?> interfaceClass) {
+        Object interfaceClassInstance = null;
+        Class<?> interfaceImplClass = findImplementation(interfaceClass);
+        Field[] fields = interfaceImplClass.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Component.class)) {
+                Object fieldInstance = getInstance(field.getType());
+                interfaceClassInstance = createNewInstance(interfaceImplClass);
+                try {
+                    field.setAccessible(true);
+                    field.set(interfaceClassInstance, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can't initialize field "
+                            + field.getName() + " in Class "
+                            + interfaceImplClass.getName());
+                }
+            }
+        }
+        return interfaceClassInstance == null ? createNewInstance(interfaceImplClass)
+                : interfaceClassInstance;
+    }
+
+    private Object createNewInstance(Class<?> interfaceClassImpl) {
+        if (!interfaceClassImpl.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException(interfaceClassImpl.getName()
+            + " is not marked with annotation for instantiation");
+        }
+        if (instances.containsKey(interfaceClassImpl)) {
+            return instances.get(interfaceClassImpl);
+        }
+        try {
+            Object instance = interfaceClassImpl.getConstructor().newInstance();
+            instances.put(interfaceClassImpl, instance);
+            return instance;
+        } catch (InstantiationException | NoSuchMethodException
+                | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException("Can't create new instance of: "
+                    + interfaceClassImpl.getName());
+        }
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClass) {
+        Map<Class<?>, Class<?>> interfaceImplementation = new HashMap<>();
+        interfaceImplementation.put(FileReaderService.class, FileReaderServiceImpl.class);
+        interfaceImplementation.put(ProductParser.class, ProductParserImpl.class);
+        interfaceImplementation.put(ProductService.class, ProductServiceImpl.class);
+        if (interfaceClass.isInterface()) {
+            return interfaceImplementation.get(interfaceClass);
+        }
+        return interfaceClass;
     }
 }
--- src/main/java/mate.academy/service/impl/ProductServiceImpl.java
+++ src/main/java/mate.academy/service/impl/ProductServiceImpl.java
@@ -2,13 +2,17 @@ package mate.academy.service.impl;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import mate.academy.lib.Component;
 import mate.academy.model.Product;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 
+@Component
 public class ProductServiceImpl implements ProductService {
+    @Component
     private ProductParser productParser;
+    @Component
     private FileReaderService fileReaderService;
 
     @Override
