e2042eba815733063ae23937de6282b695094903
Iryna Ustymenko <i.ustimenko18@gmail.com>
2021-05-07 12:09:32 +0300

    fixed method findImplementation

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -55,8 +54,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz,instance);
             return instance;
-        } catch (NoSuchMethodException | IllegalAccessException
-                | InstantiationException | InvocationTargetException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create a new instance of " + clazz.getName(), e);
         }
     }
@@ -70,10 +68,7 @@ public class Injector {
         if (interfaceImplementation.get(interfaceClazz).isAnnotationPresent(Component.class)) {
             return interfaceImplementation.get(interfaceClazz);
         }
-        if (interfaceClazz.isInterface()) {
-            throw new RuntimeException(interfaceClazz.getName() + " interface doesn't have: "
-                    + Component.class + " annotation");
-        }
-        return interfaceClazz;
+        throw new RuntimeException(interfaceClazz.getName() + " interface implementation doesn't "
+                + "have: " + Component.class + " annotation");
     }
 }
