b02b6233ed0d8c1ce2c5d5c29e2ccc0315197b43
Oksana Lashchenko <lashchenkooksana@gmail.com>
2021-05-07 15:51:07 +0300

    ReflectiveOperationException added

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -68,8 +67,7 @@ public class Injector {
             }
             throw new RuntimeException("Can't create a new instance of " + clazz.getName()
                     + ". It doesn't contain @Component annotation");
-        } catch (NoSuchMethodException | IllegalAccessException
-                | InstantiationException | InvocationTargetException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create a new instance of " + clazz.getName());
         }
     }
