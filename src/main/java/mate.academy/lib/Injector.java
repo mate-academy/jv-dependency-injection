e1fe48fcb30a65519559cfd75049eb5b030b1147
Halina Kharlanava <galina.harlanova1@gmail.com>
2021-05-07 15:12:13 +0200

    Changed 4 exceptions on line 70 to 1 parent.

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -68,8 +67,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(inputImplClass, instance);
             return instance;
-        } catch (NoSuchMethodException | InstantiationException
-                | IllegalAccessException | InvocationTargetException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create a new instance of " + inputImplClass + e);
         }
     }
