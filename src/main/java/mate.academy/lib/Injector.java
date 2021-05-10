3b0de01c359114982dd54870022c436623af9064
MakeDreamCode <aklimenchuk@gmail.com>
2021-05-10 18:44:25 +0300

    Added the check if clazz.isInterface(). Group of Exceptions was replaced by ReflectiveOperationException

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -45,7 +44,7 @@ public class Injector {
     }
 
     private Object createNewInstance(Class<?> clazz) {
-        if (!clazz.isAnnotationPresent(Component.class)) {
+        if (clazz.isInterface() && !clazz.isAnnotationPresent(Component.class)) {
             throw new RuntimeException("Can`t create the instance of "
                     + clazz.getName() + "!\n "
                     + "Class doesnt have the annotation @Component!");
@@ -58,8 +57,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (NoSuchMethodException | IllegalAccessException
-                | InstantiationException | InvocationTargetException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can`t create a new instance of " + clazz.getName());
         }
     }
