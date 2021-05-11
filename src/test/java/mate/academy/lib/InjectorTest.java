c0d6c8530c7b793ac9269579a8db6e401bf8e64c
romandubovskyi <rdubowskij@gmail.com>
2021-05-11 15:14:05 +0300

    improved tests readability

--- src/test/java/mate/academy/lib/InjectorTest.java
+++ src/test/java/mate/academy/lib/InjectorTest.java
@@ -14,24 +14,23 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 
 class InjectorTest {
-
     private Injector injector = Injector.getInjector();
 
-  @Test
-  public void component_isAnnotationPresent() {
-    boolean isPresent = Arrays.stream(Component.class.getAnnotations())
-            .anyMatch(annotation -> annotation.annotationType().equals(Retention.class));
-    Assertions.assertTrue(isPresent, "Component  "
-            + "should have Retention annotation with policy RUNTIME");
-  }
+    @Test
+    public void component_isRetentionSet() {
+        boolean isPresent = Arrays.stream(Component.class.getAnnotations())
+                .anyMatch(annotation -> annotation.annotationType().equals(Retention.class));
+        Assertions.assertTrue(isPresent, "Component  "
+                + "should have Retention annotation with policy RUNTIME");
+    }
 
-  @Test
-  public void inject_isAnnotationPresent() {
-    boolean isPresent = Arrays.stream(Inject.class.getAnnotations())
-            .anyMatch(annotation -> annotation.annotationType().equals(Retention.class));
-    Assertions.assertTrue(isPresent, "Inject  "
-            + "should have Retention annotation with policy RUNTIME");
-  }
+    @Test
+    public void inject_isRetentionSet() {
+        boolean isPresent = Arrays.stream(Inject.class.getAnnotations())
+                .anyMatch(annotation -> annotation.annotationType().equals(Retention.class));
+        Assertions.assertTrue(isPresent, "Inject  "
+                + "should have Retention annotation with policy RUNTIME");
+    }
 
     @Test
     public void fileReaderServiceImpl_isAnnotationPresent() {
@@ -60,31 +59,31 @@ class InjectorTest {
     @Test
     public void getInstance_invalidClassThrowsException() {
         Assertions.assertThrows(RuntimeException.class, () -> injector.getInstance(String.class)
-        , "If Invalid class is passed throw RuntimeException");
+                , "If Invalid class is passed throw RuntimeException");
     }
 
     @Test
     public void getInstance_productParserInstance() {
-        ProductParser actual = (ProductParser) injector.getInstance(ProductParser.class);
+        Object actual = injector.getInstance(ProductParser.class);
 
         Assertions.assertTrue(actual instanceof ProductParser,
-                "Injector can't generate instance of ProductParser");
+                "Injector should be able to generate instance of ProductParser");
     }
 
     @Test
     public void getInstance_productServiceInstance() {
-        ProductService actual = (ProductService) injector.getInstance(ProductService.class);
+        Object actual = injector.getInstance(ProductService.class);
 
         Assertions.assertTrue(actual instanceof ProductService,
-                "Injector can't generate instance of ProductParser");
+                "Injector should be able to generate instance of ProductService");
     }
 
     @Test
     public void getInstance_fileReaderInstance() {
-        FileReaderService actual = (FileReaderService) injector.getInstance(FileReaderService.class);
+        Object actual = injector.getInstance(FileReaderService.class);
 
         Assertions.assertTrue(actual instanceof FileReaderService,
-                "Injector can't generate instance of ProductParser");
+                "Injector should be able to generate instance of FileReader");
     }
 
     @Test
