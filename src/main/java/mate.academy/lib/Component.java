f70fc329839e243371d28e3e456d3e39dae092d1
Yaroslav Rybalko <yar.rybka@gmail.com>
2021-05-11 15:39:13 +0300

    2ND ATTEMPT

--- src/main/java/mate.academy/lib/Component.java
+++ src/main/java/mate.academy/lib/Component.java
@@ -2,7 +2,11 @@ package mate.academy.lib;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(value = TYPE)
 public @interface Component {
 }
--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -52,9 +51,8 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (NoSuchMethodException | InvocationTargetException
-                | InstantiationException | IllegalAccessException e) {
-            throw new RuntimeException("Can't create a new instance of " + clazz.getName(), e);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Can't create a new instance of (" + clazz.getName() + ")", e);
         }
 
     }
@@ -64,11 +62,12 @@ public class Injector {
         interfaceImplementation.put(FileReaderService.class, FileReaderServiceImpl.class);
         interfaceImplementation.put(ProductParser.class, ProductParserImpl.class);
         interfaceImplementation.put(ProductService.class, ProductServiceImpl.class);
-        if (interfaceImplementation.get(interfaceClazz).isAnnotationPresent(Component.class)) {
+        if (interfaceClazz.isInterface()
+                && interfaceImplementation.get(interfaceClazz).isAnnotationPresent(Component.class)) {
             return interfaceImplementation.get(interfaceClazz);
         }
         throw new RuntimeException("(" + interfaceClazz.getName()
-                + ") doesn't marked by annotation @Component");
+                + ") is not interface or doesn't marked by annotation @Component");
 
     }
 }
