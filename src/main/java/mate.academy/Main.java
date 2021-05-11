40fe76bb3072db684512effa5a6d1d5257aec790
SerKisJD <sergej.kislyakov@gmail.com>
2021-05-11 09:42:29 +0300

    3 injector takes an abstraction now

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -4,7 +4,6 @@ import java.util.List;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
 import mate.academy.service.ProductService;
-import mate.academy.service.impl.ProductServiceImpl;
 
 public class Main {
 
@@ -12,7 +11,7 @@ public class Main {
         // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
         ProductService productService = (ProductService) injector
-                .getInstance(ProductServiceImpl.class);
+                .getInstance(ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
