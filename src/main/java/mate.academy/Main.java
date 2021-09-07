cd721fc8744bbff6daf33d9fc9b777b67631a4c8
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2021-09-07 17:02:11 +0300

    used ProductService interface instead of implementation in main

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -4,15 +4,13 @@ import java.util.List;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
 import mate.academy.service.ProductService;
-import mate.academy.service.impl.ProductServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
         ProductService productService = (ProductService) injector
-                .getInstance(ProductServiceImpl.class);
+                .getInstance(ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
