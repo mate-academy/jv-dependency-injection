1968d7223bbce0dea6dd510d072893fc79a6032c
mishamolnar <michaellmuller423@gmail.com>
2021-09-11 14:51:32 +0300

    injector created

--- src/main/java/mate.academy/service/impl/ProductParserImpl.java
+++ src/main/java/mate.academy/service/impl/ProductParserImpl.java
@@ -1,16 +1,19 @@
 package mate.academy.service.impl;
 
 import java.math.BigDecimal;
+
+import mate.academy.lib.Component;
 import mate.academy.model.Product;
 import mate.academy.service.ProductParser;
 
+@Component
 public class ProductParserImpl implements ProductParser {
-
     public static final int ID_POSITION = 0;
     public static final int NAME_POSITION = 1;
     public static final int CATEGORY_POSITION = 2;
     public static final int DESCRIPTION_POSITION = 3;
     public static final int PRICE_POSITION = 4;
+
 
     @Override
     public Product parse(String productInfo) {
@@ -21,6 +24,7 @@ public class ProductParserImpl implements ProductParser {
         product.setCategory(data[CATEGORY_POSITION]);
         product.setDescription(data[DESCRIPTION_POSITION]);
         product.setPrice(new BigDecimal(data[PRICE_POSITION]));
+
         return product;
     }
 }
