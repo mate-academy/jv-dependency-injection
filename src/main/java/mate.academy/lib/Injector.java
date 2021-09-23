f8cdef02eb2242a74685ed3289ad46fb4cef7c23
kryzhen <krutozavr@yahoo.com>
2021-09-23 23:34:12 +0300

    injector-hm after review

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -23,16 +22,17 @@ public class Injector {
         Object clazzImplInstance = null;
         Class<?> clazz = findImplementation(interfaceClazz);
         Field[] declaredFields = clazz.getDeclaredFields();
-        if (interfaceClazz.isAnnotationPresent(Component.class)) {
+        if (clazz.isAnnotationPresent(Component.class)) {
             clazzImplInstance = createNewInstance(clazz);
             for (Field field : declaredFields) {
                 if (field.isAnnotationPresent(Inject.class)) {
-                    clazzImplInstance = createNewInstance(clazz);
                     Object fieldInstance = getInstance(field.getType());
                     try {
+                        field.setAccessible(true);
                         field.set(clazzImplInstance, fieldInstance);
                     } catch (IllegalAccessException e) {
-                        e.printStackTrace();
+                        throw new RuntimeException("Unable to create instance for this field: "
+                                + field.getName() + "For class: " + clazz.getName());
                     }
                 }
             }
@@ -58,9 +58,8 @@ public class Injector {
         try {
             Constructor<?> constructor = clazz.getConstructor();
             return constructor.newInstance();
-        } catch (InstantiationException | InvocationTargetException
-                | IllegalAccessException | NoSuchMethodException e) {
-            throw new RuntimeException("Can't create a new instance of " + clazz);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Can't create a new instance of " + clazz.getName());
         }
     }
 }
--- src/main/java/mate.academy/service/FileReaderService.java
+++ src/main/java/mate.academy/service/FileReaderService.java
@@ -1,9 +1,7 @@
 package mate.academy.service;
 
 import java.util.List;
-import mate.academy.lib.Component;
 
-@Component
 public interface FileReaderService {
     List<String> readFile(String fileName);
 }
