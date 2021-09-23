c6c0330b5f697d9a2e5a033afb8bfccca0d4abc4
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-09-23 19:09:20 +0300

    small fixes

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -64,14 +64,16 @@ public class Injector {
             if (interfacesImplMap.containsKey(interfaceClazz)) {
                 return interfacesImplMap.get(interfaceClazz).stream()
                         .findFirst()
-                        .get();
+                        .orElseThrow(() -> new RuntimeException("There are"
+                                + " no classes that implement this interface: " + interfaceClazz.getName()));
             }
             Reflections reflections = new Reflections("mate.academy.service");
             Set<Class<?>> classes = reflections.getSubTypesOf((Class<Object>) interfaceClazz);
             interfacesImplMap.put(interfaceClazz, classes);
             return classes.stream()
                     .findFirst()
-                    .get();
+                    .orElseThrow(() -> new RuntimeException("There are"
+                            + " no classes that implement this interface: " + interfaceClazz.getName()));
         }
         return interfaceClazz;
     }
