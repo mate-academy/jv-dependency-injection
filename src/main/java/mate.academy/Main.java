0ba82ddd2418ad6956c43cd323b8d16cfb1e6868
SolnalarmJulia <solnalarm@meta.ua>
2021-09-26 16:55:02 +0300

    creating injector

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -1,6 +1,5 @@
 package mate.academy;
 
-import java.util.List;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
 import mate.academy.service.ProductService;
@@ -10,8 +9,10 @@ public class Main {
     public static void main(String[] args) {
         // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
-        ProductService productService = null;
-        List<Product> products = productService.getAllFromFile("products.txt");
-        products.forEach(System.out::println);
+        ProductService productService = (ProductService) injector.getInstance(ProductService.class);
+        productService.getAllFromFile("src/main/resources/products.txt");
+        for (Product p : productService.getAllFromFile("src/main/resources/products.txt")) {
+            System.out.println(p.toString());
+        }
     }
 }
