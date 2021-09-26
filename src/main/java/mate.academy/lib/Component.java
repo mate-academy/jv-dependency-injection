eada17ef407fd0d44027a735d127c697714af6be
Dmytro Heleta <dmytroheleta@gmail.com>
2021-09-26 18:25:25 +0300

    implement injector

--- src/main/java/mate.academy/lib/Component.java
+++ src/main/java/mate.academy/lib/Component.java
@@ -1,5 +1,11 @@
 package mate.academy.lib;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+
 public @interface Component {
 
 }
+
