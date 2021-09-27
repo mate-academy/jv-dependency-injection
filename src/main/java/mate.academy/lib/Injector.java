9970e25ac9e3a4f6e2fefc53b04607df76411fc0
pustoj0 <pustoj0@gmail.com>
2021-09-28 02:15:37 +0300

    deleted method isComponent as logic told that method isAnnotationPresent exist for Class as well. implemented method getInstance.

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,31 +1,61 @@
 package mate.academy.lib;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Injector {
     private static final Injector injector = new Injector();
+
+    private Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClass) {
-        Class<?> implementationClass = ImplementationMap.get(interfaceClass);
-        if (!isComponent(implementationClass)) {
-            throw new RuntimeException("Injection failed, missing @Component annotaion on the class "
-                    + implementationClass.getName());
+        Class<?> classImplementation = ImplementationMap.get(interfaceClass);
+        if (!classImplementation.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Injection failed, missing annotation @Component on the class "
+                    + classImplementation.getName());
         }
-        return null;
-    }
-
-    private boolean isComponent(Class<?> implementationClass) {
-        Annotation[] classAnnotations = implementationClass.getAnnotations();
-        for (Annotation annotation : classAnnotations) {
-            if (annotation instanceof Component) {
-                return true;
+        Field[] fields = classImplementation.getFields();
+        Object classImplementationInstance = null;
+        for (Field field: fields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Object fieldInstance = getInstance(field.getType());
+                classImplementationInstance = createNewInstance(classImplementation);
+                try {
+                    field.setAccessible(true);
+                    field.set(classImplementationInstance, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can't initialize field value! "
+                    + "Class: " + classImplementation.getName() + ". Field: " + field.getName());
+                }
             }
         }
-        return false;
+        if (classImplementationInstance == null) {
+            classImplementationInstance = createNewInstance(classImplementation);
+        }
+        return classImplementationInstance;
     }
 
+    private Object createNewInstance(Class<?> implementationClass) {
+        if (instances.containsKey(implementationClass)) {
+            return instances.get(implementationClass);
+        }
+        try {
+            Constructor<?> constructor = implementationClass.getConstructor();
+            Object instance = constructor.newInstance();
+            instances.put(implementationClass, instance);
+            return instance;
+        } catch (InstantiationException | InvocationTargetException
+                | NoSuchMethodException | IllegalAccessException e) {
+            throw new RuntimeException("Can't create inctance of class "
+                    + implementationClass.getName(), e);
+        }
+    }
 }
