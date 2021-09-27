8ad6f75562886698e3e95dce063c694e01e4089a
Serhii Orlov <orlov30@gmail.com>
2021-09-27 15:25:59 +0300

    change type of exception

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -62,7 +62,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (Exception e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create new instance of " + clazz.getName(), e);
         }
     }
