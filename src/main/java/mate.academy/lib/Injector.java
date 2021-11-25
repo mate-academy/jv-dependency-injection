097e5cf17d532c964489944a7fe64e5db1887cac
Olha Tankevych <freakystyley85@gmail.com>
2021-11-25 21:25:58 +0200

    deleted empty line and changed 4 exceptions to 1

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -56,8 +55,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException
-                | IllegalAccessException e) {
+        } catch (Exception e) {
             throw new RuntimeException("Can't create a new instance of " + clazz.getName());
         }
     }
