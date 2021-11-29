758cf99a1b51d81ed4b9701e99f4374ef194ef46
farfardesu <savavlad003@gmail.com>
2021-11-29 21:11:47 +0200

    suggested changes were implemented

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -22,7 +22,9 @@ public class Injector {
     public Object getInstance(Class<?> interfaceClazz) {
         Class<?> clazz = findImplementation(interfaceClazz);
         if (!clazz.isAnnotationPresent(Component.class)) {
-            throw new RuntimeException("Annotation \"Component\" is not present");
+            throw new RuntimeException("Injection failed, " +
+                    "missing @Component annotaion on the class \""
+                     + clazz.getName());
         }
         Field[] declaredFields = interfaceClazz.getDeclaredFields();
         Object clazzImplementationInstance = null;
