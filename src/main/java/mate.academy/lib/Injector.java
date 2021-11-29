7691f1314debcc5900717d64a103586e954b4e69
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-11-29 08:29:53 +0200

    mvn clean package success

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
-
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
