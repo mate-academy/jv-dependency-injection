cbf78fecffb9e0fa1ff56927199dcf81b4b3fee5
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2021-12-03 19:55:48 +0200

    In the class Injector implemented method getInstance; added methods: createNewInstance, getImpl

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,76 @@
 package mate.academy.lib;
 
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Injector {
     private static final Injector injector = new Injector();
-
+    private static Map<Class<?>, Object> instances;
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+
+        Class<?> implClazz = getImpl(interfaceClazz);
+        Object clazzImplementationObject = createNewInstance(implClazz);
+        if (!implClazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Annotation @Component is not present in the class: "
+                    + implClazz.getName());
+        }
+        Field[] declaredFields = implClazz.getDeclaredFields();
+        for (Field field: declaredFields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Object fieldInstance = getInstance(field.getType());
+                field.setAccessible(true);
+                try {
+                    field.set(clazzImplementationObject, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can't set the field: \"" + field.getName()
+                            + "\" of the class: \"" + implClazz.getName() + "\"");
+                }
+
+            }
+        }
+        return clazzImplementationObject;
+    }
+
+    private Object createNewInstance(Class<?> implClazz) {
+        if (instances == null) {
+            instances = new HashMap<>();
+        } else if (instances.containsKey(implClazz)) {
+            return instances.get(implClazz);
+        }
+        try {
+            Constructor<?> constructor = implClazz.getConstructor();
+            Object instance = constructor.newInstance();
+            instances.put(implClazz, instance);
+            return instance;
+
+        } catch (NoSuchMethodException
+                | IllegalAccessException
+                | InstantiationException
+                | InvocationTargetException e) {
+            throw new RuntimeException("Can't create new object of the class: "
+                    +implClazz.getName(), e);
+        }
+    }
+
+    private Class<?> getImpl(Class<?> interfaceClazz) {
+        Map<Class<?>, Class<?>> clazzImplementationMap = new HashMap<>();
+        clazzImplementationMap.put(FileReaderService.class, FileReaderServiceImpl.class);
+        clazzImplementationMap.put(ProductParser.class, ProductParserImpl.class);
+        clazzImplementationMap.put(ProductService.class, ProductServiceImpl.class);
+        return clazzImplementationMap.get(interfaceClazz);
     }
 }
