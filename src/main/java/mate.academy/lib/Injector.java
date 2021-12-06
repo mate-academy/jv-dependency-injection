b92f3db78c7456b60ab6290e23551d9a0fe740ae
Noct2000 <oleg.kvasha.00@gmail.com>
2021-12-06 19:17:08 +0200

    Fixed caching the exception in the Injector

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -58,10 +57,7 @@ public class Injector {
             instances.put(implClazz, instance);
             return instance;
 
-        } catch (NoSuchMethodException
-                | IllegalAccessException
-                | InstantiationException
-                | InvocationTargetException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create new object of the class: "
                     + implClazz.getName(), e);
         }
