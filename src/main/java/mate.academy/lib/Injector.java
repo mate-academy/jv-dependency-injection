67884eea3df66cc1d3a9254094c32e8f8594195d
vadim9482 <vadim9482@gmail.com>
2021-12-21 00:52:01 +0200

    Injector: check for interface was removed, reflection exceptions classes were replaced with one parent exception Reflection exception

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -23,7 +22,7 @@ public class Injector {
     public Object getInstance(Class<?> interfaceClazz) {
         Object clazzImplementationInstance = null;
         Class<?> clazz = findImplementation(interfaceClazz);
-        if (!clazz.isAnnotationPresent(Component.class) && !clazz.isInterface()) {
+        if (!clazz.isAnnotationPresent(Component.class)) {
             throw new RuntimeException("Class isn't component " + clazz.getName());
         }
         Field[] declaredFields = clazz.getDeclaredFields();
@@ -55,8 +54,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (NoSuchMethodException | InstantiationException
-                | IllegalAccessException | InvocationTargetException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create a new instance of " + clazz.getName());
         }
     }
