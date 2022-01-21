1720c7275c048d5f8f4046c82382aca78d1dae0a
Maksym Pokras <mxpokras@gmail.com>
2022-01-21 14:44:11 +0200

    Checkstyle violations are eliminated

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,18 +1,17 @@
 package mate.academy.lib;
 
-import mate.academy.service.FileReaderService;
-import mate.academy.service.ProductParser;
-import mate.academy.service.ProductService;
-import mate.academy.service.impl.FileReaderServiceImpl;
-import mate.academy.service.impl.ProductParserImpl;
-import mate.academy.service.impl.ProductServiceImpl;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
@@ -43,9 +42,9 @@ public class Injector {
                 try {
                     field.set(instance, fieldValue);
                 } catch (IllegalAccessException e) {
-                    throw new RuntimeException("Can't initialize '" +
-                            implClass.getSimpleName() + "." +
-                            field.getName(), e);
+                    throw new RuntimeException("Can't initialize '"
+                            + implClass.getSimpleName() + "."
+                            + field.getName(), e);
                 }
             }
         }
@@ -61,8 +60,8 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(implClass, instance);
             return instance;
-        } catch (NoSuchMethodException | InvocationTargetException |
-                InstantiationException | IllegalAccessException e) {
+        } catch (NoSuchMethodException | InvocationTargetException
+                | InstantiationException | IllegalAccessException e) {
             throw new RuntimeException("Can't create '" + implClass.getName() + "' instance", e);
         }
     }
