228f636947db04533704220156517c47c6c5ef56
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2022-02-20 22:03:39 +0200

    Performed refactoring, logic errors and checkstyle corrections

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,5 +1,10 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
@@ -7,19 +12,15 @@ import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Injector {
     private static final Map<Class<?>, Class<?>> IMPLEMENTATIONS = new HashMap<>();
+
     static {
         IMPLEMENTATIONS.put(ProductService.class, ProductServiceImpl.class);
         IMPLEMENTATIONS.put(ProductParser.class, ProductParserImpl.class);
         IMPLEMENTATIONS.put(FileReaderService.class, FileReaderServiceImpl.class);
     }
+
     private static final Map<Class<?>, Object> INSTANCES = new HashMap<>();
     private static final Injector injector = new Injector();
 
@@ -29,12 +30,12 @@ public class Injector {
 
     public Object getInstance(Class<?> interfaceClass) {
         Object classImplementationInstance = null;
-        Class<?> aClass = IMPLEMENTATIONS.get(interfaceClass);
-        Field[] classFields = aClass.getDeclaredFields();
+        Class<?> implementationClass = findImplementation(interfaceClass);
+        Field[] classFields = implementationClass.getDeclaredFields();
         for (Field field : classFields) {
             if (field.isAnnotationPresent(Inject.class)) {
                 Object fieldInstance = getInstance(field.getType());
-                classImplementationInstance = createNewInstance(aClass);
+                classImplementationInstance = createNewInstance(implementationClass);
 
                 try {
                     field.setAccessible(true);
@@ -42,40 +43,46 @@ public class Injector {
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException(
                             "Cannot set field '" + field.getName()
-                                    + "' in class '" + aClass.getName() + "'.", e);
+                                    + "' in class '" + implementationClass.getName() + "'.", e);
                 }
             }
         }
         if (classImplementationInstance == null) {
-            classImplementationInstance = createNewInstance(aClass);
+            classImplementationInstance = createNewInstance(implementationClass);
         }
         return classImplementationInstance;
     }
 
-    private Object createNewInstance(Class<?> aClass) {
-        if (INSTANCES.containsKey(aClass)) {
-            return INSTANCES.get(aClass);
+    private Object createNewInstance(Class<?> implementationClass) {
+        if (INSTANCES.containsKey(implementationClass)) {
+            return INSTANCES.get(implementationClass);
         }
         try {
-            Constructor<?> constructor = aClass.getConstructor();
+            Constructor<?> constructor = implementationClass.getConstructor();
             Object instance = constructor.newInstance();
-            INSTANCES.put(aClass, instance);
+            INSTANCES.put(implementationClass, instance);
             return instance;
         } catch (NoSuchMethodException | InvocationTargetException
                 | InstantiationException | IllegalAccessException e) {
-            throw new RuntimeException("Can't create instance of class '" + aClass.getName() + "'.", e);
+            throw new RuntimeException("Can't create instance of class '"
+                    + implementationClass.getName() + "'.", e);
         }
     }
 
     private Class<?> findImplementation(Class<?> interfaceClass) {
-        if (interfaceClass.isInterface() && IMPLEMENTATIONS.containsKey(interfaceClass)) {
-            Class<?> aClass = IMPLEMENTATIONS.get(interfaceClass);
-            if (!interfaceClass.isAnnotationPresent(Component.class)) {
+        if (!IMPLEMENTATIONS.containsKey(interfaceClass)) {
+            throw new RuntimeException(
+                    "Using improper component. Cannot find implementation for interface '"
+                            + interfaceClass.getName() + "'.");
+        }
+        if (interfaceClass.isInterface()) {
+            Class<?> implementationClass = IMPLEMENTATIONS.get(interfaceClass);
+            if (!implementationClass.isAnnotationPresent(Component.class)) {
                 throw new RuntimeException(
-                        "Using improper class. There is no annotation 'Component' on class '"
-                                + interfaceClass.getName() + "'.");
+                        "Using improper component. There is no annotation 'Component' on class '"
+                                + implementationClass.getName() + "'.");
             }
-            return aClass;
+            return implementationClass;
         }
         return interfaceClass;
     }
