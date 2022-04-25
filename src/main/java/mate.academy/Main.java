2537d0f529fc88d0da4b9bb3efd6aa3bf2ab5ac4
yuriikozar <yuriikozar@gmail.com>
2022-04-25 17:41:33 +0300

    Commit 1 Solution + Checkstyle v1

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -3,6 +3,7 @@ package mate.academy;
 import java.util.List;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
+import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 
 public class Main {
@@ -10,7 +11,9 @@ public class Main {
     public static void main(String[] args) {
         // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
-        ProductService productService = null;
+        ProductService productService = (ProductService) injector.getInstance(ProductService.class);
+        ProductParser productParser = (ProductParser) injector.getInstance(ProductParser.class);
+        Product product = (Product) injector.getInstance(Product.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
