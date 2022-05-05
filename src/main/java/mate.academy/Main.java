c7bd512e387909340ccfc9cdde752597d5483a25
GrabenkoVitaliy <grabenkovitaliy7@gmail.com>
2022-05-05 22:09:50 +0300

    first version of HW

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -1,17 +1,17 @@
-package mate.academy;
-
-import java.util.List;
-import mate.academy.lib.Injector;
-import mate.academy.model.Product;
-import mate.academy.service.ProductService;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // Please test your Injector here. Feel free to push this class as a part of your solution
-        Injector injector = Injector.getInjector();
-        ProductService productService = null;
-        List<Product> products = productService.getAllFromFile("products.txt");
-        products.forEach(System.out::println);
-    }
-}
+//package mate.academy;
+//
+//import java.util.List;
+//import mate.academy.lib.Injector;
+//import mate.academy.model.Product;
+//import mate.academy.service.ProductService;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        // Please test your Injector here. Feel free to push this class as a part of your solution
+//        Injector injector = Injector.getInjector();
+//        ProductService productService = null;
+//        List<Product> products = productService.getAllFromFile("products.txt");
+//        products.forEach(System.out::println);
+//    }
+//}
