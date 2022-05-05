0ba07de31b6b628c012b7ffa83222a0fa1cdd752
Volodymyr Klimyuk <klimyuk054@gmail.com>
2022-05-05 22:57:06 +0300

    Fixed some problem.

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -8,5 +8,9 @@ import mate.academy.service.ProductService;
 public class Main {
 
     public static void main(String[] args) {
+        Injector injector = Injector.getInjector();
+        ProductService productService = (ProductService) injector.getInstance(ProductService.class);
+        List<Product> products = productService.getAllFromFile("products.txt");
+        products.forEach(System.out::println);
     }
 }
