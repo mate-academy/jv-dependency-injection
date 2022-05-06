c9d3f73c07d164757c4fa2030b60546183d30703
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-05-06 13:48:10 +0300

    Fields have been injected.

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
-        ProductService productService = null;
+        ProductService productService = (ProductService) injector.getInstance(ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
