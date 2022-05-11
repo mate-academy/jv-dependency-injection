b6e4c7871aa81b061fa60f5761a41abf98e8cfce
Denys.Sh <7860denis@gmail.com>
2022-05-11 22:14:22 +0300

    magic inside

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,90 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> clazz = findImplementation(interfaceClazz);
+        Object clazzImplementationInstance = createNewInstance(clazz);
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Object fieldInstance = getInstance(field.getType());
+                clazzImplementationInstance = createNewInstance(clazz);
+                try {
+                    field.setAccessible(true);
+                    field.set(clazzImplementationInstance, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can`t initialize field value. "
+                            + "Class: " + clazz.getName()
+                            + ". Field: " + field.getName(), e);
+                }
+            }
+        }
+        if (clazzImplementationInstance == null) {
+            clazzImplementationInstance = createNewInstance(clazz);
+        }
+        return clazzImplementationInstance;
+    }
+
+    private Object createNewInstance(Class<?> clazz) {
+        if (instances.containsKey(clazz)) {
+            return instances.get(clazz);
+        }
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            Object instance = constructor.newInstance();
+            instances.put(clazz, instance);
+            return instance;
+        } catch (NoSuchMethodException | InvocationTargetException
+                | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("Can`t create a "
+                    + "new instance of " + clazz.getName(), e);
+        }
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
+        interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
+        interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
+        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
+        if (interfaceClazz.isInterface()) {
+            return interfaceImplementations.get(interfaceClazz);
+        }
+        return findImplementationInterface(interfaceClazz);
+    }
+
+    private Class<?> findImplementationInterface(Class<?> interfaceClazz) {
+        if (!interfaceClazz.isInterface()
+                && interfaceClazz.isAnnotationPresent(Component.class)) {
+            return interfaceClazz;
+        }
+        Class<?>[] interfaces = interfaceClazz.getInterfaces();
+        for (Class<?> singleInterface : interfaces) {
+            if (singleInterface.equals(interfaceClazz)
+                    && interfaceClazz.isAnnotationPresent(Component.class)) {
+                return interfaceClazz;
+            }
+        }
+        throw new RuntimeException("Can`t find class which implements "
+                + interfaceClazz.getName()
+                + " interface and has valid annotation @Component");
     }
 }
--- src/main/java/mate.academy/service/impl/ProductServiceImpl.java
+++ src/main/java/mate.academy/service/impl/ProductServiceImpl.java
@@ -2,17 +2,27 @@ package mate.academy.service.impl;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import mate.academy.lib.Component;
+import mate.academy.lib.Inject;
 import mate.academy.model.Product;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 
+@Component
 public class ProductServiceImpl implements ProductService {
-    private ProductParser productParser;
+    @Inject
     private FileReaderService fileReaderService;
+    @Inject
+    private ProductParser productParser;
 
     @Override
     public List<Product> getAllFromFile(String filePath) {
+        if (fileReaderService == null || productParser == null) {
+            throw new RuntimeException("Can`t get all product because this "
+                    + productParser + " or " + fileReaderService
+                    + " is null");
+        }
         return fileReaderService.readFile(filePath)
                 .stream()
                 .map(productParser::parse)
