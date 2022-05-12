d0835bd2f4c198cb48dbf399469df48c9059ce9c
Vadym Datsiuk <lutskvadym@gmail.com>
2022-05-12 16:50:53 +0300

    getters were added

--- src/main/java/mate.academy/model/Product.java
+++ src/main/java/mate.academy/model/Product.java
@@ -13,24 +13,40 @@ public class Product {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public Long getId() { 
+        return id; 
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
     }
 
     @Override
