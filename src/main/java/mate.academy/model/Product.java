44765e1ec926d64142a32d125bef2027aff67655
Vadym Datsiuk <lutskvadym@gmail.com>
2022-05-12 14:57:45 +0300

    for approving, checked

--- src/main/java/mate.academy/model/Product.java
+++ src/main/java/mate.academy/model/Product.java
@@ -9,10 +9,6 @@ public class Product {
     private String description;
     private BigDecimal price;
 
-    public Long getId() {
-        return id;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -25,24 +21,12 @@ public class Product {
         this.name = name;
     }
 
-    public String getCategory() {
-        return category;
-    }
-
     public void setCategory(String category) {
         this.category = category;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
     }
 
     public void setPrice(BigDecimal price) {
