daf39c842a01ca40279a66d773de38d18da4cb61
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-06-17 11:00:47 +0300

    сделал не совсем так, как предлагалось в уроке, но вроде получилось даже проще

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,5 +1,17 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
 
@@ -8,6 +20,41 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> clazz = findImplementation(interfaceClazz);
+        if (!clazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Unsupported class");
+        }
+        Object clazzImplInstance = null;
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            clazzImplInstance = constructor.newInstance();
+        } catch (NoSuchMethodException | InstantiationException
+                 | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Can`t create new instance of " + clazz.getSimpleName(), e);
+        }
+
+        for (Field declaredField : clazz.getDeclaredFields()) {
+            if (declaredField.isAnnotationPresent(Inject.class)) {
+                Object fieldInstance = getInstance(declaredField.getType());
+                declaredField.setAccessible(true);
+                try {
+                    declaredField.set(clazzImplInstance, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can`t set value for " + declaredField.getName(), e);
+                }
+            }
+        }
+        return clazzImplInstance;
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        Map<Class<?>, Class<?>> interfaceImpl = new HashMap<>();
+        interfaceImpl.put(FileReaderService.class, FileReaderServiceImpl.class);
+        interfaceImpl.put(ProductService.class, ProductServiceImpl.class);
+        interfaceImpl.put(ProductParser.class, ProductParserImpl.class);
+        if (interfaceImpl.containsKey(interfaceClazz)) {
+            return interfaceImpl.get(interfaceClazz);
+        }
+        return interfaceClazz;
     }
 }
