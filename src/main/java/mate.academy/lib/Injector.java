a8df2bb9e2ae2ed332e2b1063f4accfd8280e411
Igor <ZIM20093@gmail.com>
2022-06-18 02:22:16 +0300

    try to make instances map from classes using target directory

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,19 +1,33 @@
 package mate.academy.lib;
 
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import mate.academy.service.FileReaderService;
-import mate.academy.service.ProductParser;
-import mate.academy.service.ProductService;
-import mate.academy.service.impl.FileReaderServiceImpl;
-import mate.academy.service.impl.ProductParserImpl;
-import mate.academy.service.impl.ProductServiceImpl;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Injector {
+    private static final String SERVICE_INTERFACE_PACKAGE_PATH =
+            "target/classes/mate/academy/service";
+    private static final String SERVICE_INSTANCE_PACKAGE_PATH =
+            "target/classes/mate/academy/service/impl";
     private static final Injector injector = new Injector();
     private static final Map<Class<?>, Object> instances = new HashMap<>();
+    private static Map<Class<?>, Class<?>> interfaceImplementationMap;
+
+    {
+        List<Class<?>> interfacesList = getListOfClass(Path.of(SERVICE_INTERFACE_PACKAGE_PATH));
+        List<Class<?>> implementsList = getListOfClass(Path.of(SERVICE_INSTANCE_PACKAGE_PATH));
+        interfaceImplementationMap = IntStream.range(0, interfacesList.size())
+                .boxed()
+                .collect(Collectors.toMap(interfacesList::get, implementsList::get));
+        checkInstancesMap(interfaceImplementationMap);
+    }
 
     public static Injector getInjector() {
         return injector;
@@ -56,10 +70,36 @@ public class Injector {
         if (interfaceClazz.isInstance(interfaceClazz)) {
             return interfaceClazz;
         }
-        Map<Class<?>, Class<?>> interfaceImplementationMap = new HashMap<>();
-        interfaceImplementationMap.put(FileReaderService.class, FileReaderServiceImpl.class);
-        interfaceImplementationMap.put(ProductParser.class, ProductParserImpl.class);
-        interfaceImplementationMap.put(ProductService.class, ProductServiceImpl.class);
         return interfaceImplementationMap.get(interfaceClazz);
+    }
+
+    private List<Class<?>> getListOfClass(Path path) {
+        List<Path> pathList;
+        try {
+            pathList = Files.list(path).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("Can`t open path of package: " + path, e);
+        }
+        return pathList.stream().map(Path::toString)
+                .filter(s -> s.contains(".class"))
+                .map(s -> s.replace("target\\classes\\", "")
+                        .replace(".class", "").replace("\\","."))
+                .map(s -> {
+                    try {
+                        return Class.forName(s);
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException("Can`t create class type with name: " + s, e);
+                    }
+                })
+                .collect(Collectors.toList());
+    }
+
+    private void checkInstancesMap(Map<Class<?>, Class<?>> map) {
+        map.forEach((key, value) -> {
+            if (key.isInstance(value)) {
+                throw new RuntimeException("Some problem with creating map of instances. "
+                        + "Interface: " + key + " and instance: " + value);
+            }
+        });
     }
 }
