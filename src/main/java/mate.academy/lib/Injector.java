daca899631e5fd43fe886b48c69e691565b4e872
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-06-21 00:32:18 +0300

    Implement Injector class

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,78 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ServiceLoader;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private static final Map<Class<?>, Object> instancesMap = new HashMap<>();
+    private static final Map<Class<?>, Class<?>> interfacesImplementationClassMap = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> implementationClazz = findImplementationClass(interfaceClazz);
+        /* It seems to be better to move this check to the 'findImplementationClass()' method
+        before filling the 'interfacesImplementationClassMap', but then one test fails */
+        if (!implementationClazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException(
+                    "Injection failed, missing @Component annotation on the class "
+                            + implementationClazz.getSimpleName());
+        }
+        Field[] implementationClazzFields = implementationClazz.getDeclaredFields();
+        Object implementationClazzInstance = null;
+        for (Field implementationClazzField : implementationClazzFields) {
+            if (implementationClazzField.isAnnotationPresent(Inject.class)) {
+                Object implementationClazzFieldInstance =
+                        getInstance(implementationClazzField.getType());
+                implementationClazzInstance = createNewInstance(implementationClazz);
+                implementationClazzField.setAccessible(true);
+                try {
+                    implementationClazzField.set(
+                            implementationClazzInstance, implementationClazzFieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can't initialize field value. "
+                            + "Class: " + implementationClazz.getName()
+                            + ". Field: " + implementationClazzField.getName(), e);
+                }
+            }
+        }
+        if (implementationClazzInstance == null) {
+            implementationClazzInstance = createNewInstance(implementationClazz);
+        }
+        return implementationClazzInstance;
+    }
+
+    private Object createNewInstance(Class<?> implementationClazz) {
+        if (instancesMap.containsKey(implementationClazz)) {
+            return instancesMap.get(implementationClazz);
+        }
+        try {
+            Constructor<?> constructor = implementationClazz.getConstructor();
+            Object instance = constructor.newInstance();
+            instancesMap.put(implementationClazz, instance);
+            return instance;
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(
+                    "Can't create a new instance of " + implementationClazz.getName(), e);
+        }
+    }
+
+    private Class<?> findImplementationClass(Class<?> interfaceClazz) {
+        if (interfacesImplementationClassMap.containsKey(interfaceClazz)) {
+            return interfacesImplementationClassMap.get(interfaceClazz);
+        }
+        ServiceLoader<?> implementationsLoader = ServiceLoader.load(interfaceClazz);
+        for (Object implementation : implementationsLoader) {
+            Class<?> implementationClazz = implementation.getClass();
+            interfacesImplementationClassMap.put(interfaceClazz, implementationClazz);
+            return implementationClazz;
+        }
+        return interfaceClazz;
     }
 }
