5b945abe2c6c84f211a3841575e22fe2aebd3dfe
Ilya <zimmboco@gmail.com>
2022-07-14 13:40:24 +0300

    My homeWork 19 first version

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -34,7 +34,6 @@ public class Injector {
 
                 clazzImplementationInstance = createNewInstance(clazz);
 
-
                 try {
                     field.setAccessible(true);
                     field.set(clazzImplementationInstance, fieldInstance);
