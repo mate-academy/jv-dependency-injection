9afbda8e0d9cccf70483bce7061c547f7d3d0a5f
Aleksandr Savchenko <savchenko.arts@gmail.com>
2022-07-28 13:17:23 +0300

    HW-Dependency-injection

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -1,17 +1,19 @@
 package mate.academy;
 
-import java.util.List;
 import mate.academy.lib.Injector;
-import mate.academy.model.Product;
-import mate.academy.service.ProductService;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         // Please test your Injector here. Feel free to push this class as a part of your solution
-        Injector injector = Injector.getInjector();
-        ProductService productService = null;
-        List<Product> products = productService.getAllFromFile("products.txt");
-        products.forEach(System.out::println);
+        Injector injector = Injector.getInstance("mate.academy");
+//        ProductService productService = (ProductService) injector.getInstance(ProductServiceImpl.class);
+//        List<Product> products = productService.getAllFromFile("products.txt");
+//        products.forEach(System.out::println);
     }
 }
--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,108 @@
 package mate.academy.lib;
 
-public class Injector {
-    private static final Injector injector = new Injector();
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+import java.util.Enumeration;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    public static Injector getInjector() {
+public class Injector {
+    private static Map<String, Injector> injectors = new HashMap<>();
+    private Map<Class<?>, Object> instances = new HashMap<>();
+    private List<Class<?>> classes = new ArrayList<>();
+
+    public Injector(String mainPackageName) {
+        classes.addAll(getClasses(mainPackageName));
+    }
+
+    private static List<Class<?>> getClasses(String packageName)
+            throws IOException, ClassNotFoundException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        if (classLoader == null) {
+            throw new RuntimeException("Class loader is null");
+        }
+        String path = packageName.replace(".", "/");
+        Enumeration<URL> resources = classLoader.getResource(path);
+        List<File> dirs = new ArrayList<>();
+        while(resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        List<Class<?>> classes = new ArrayList<>();
+        for (File directory : dirs) {
+            classes.addAll(findClasses(directory, packageName));
+        }
+        return classes;
+    }
+
+
+    public static Injector getInstance(String mainPackageName) {
+        if (injectors.containsKey(mainPackageName)) {
+            injectors.get(mainPackageName);
+        }
+        Injector injector = new Injector(mainPackageName);
+        injectors.put(mainPackageName, injector);
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> clazz = findImplementation(interfaceClazz);
+        Object clazzImplementationInstance = null;
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Object fieldInstance = getInstance(field.getType());
+                clazzImplementationInstance = createNewInstance(clazz);
+                try {
+                    field.setAccessible(true);
+                    field.set(clazzImplementationInstance, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can not initialize field value. Class: "
+                            + clazz.getName() + ". Field: " + field.getName());
+                }
+            }
+        }
+        if (clazzImplementationInstance == null) {
+            clazzImplementationInstance = createNewInstance(clazz);
+        }
+        return clazzImplementationInstance;
+    }
+
+    private Object createNewInstance(Class<?> clazz) {
+        if (instances.containsKey(clazz)) {
+            instances.get(clazz);
+        }
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            Object instance = constructor.newInstance();
+            instances.put(clazz, instance);
+            return instance;
+        } catch (NoSuchMethodException | InvocationTargetException
+                 | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("Can not create new instance of " + clazz.getName());
+        }
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
+        interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
+        interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
+        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
+        if (interfaceClazz.isInterface()) {
+            return interfaceImplementations.get(interfaceClazz);
+        }
+        return interfaceClazz;
     }
 }
