404726ce8e8e9a623e438e2f7e1d97c05db47fee
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-08-25 17:09:57 +0300

    The injector is developed

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,72 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private static final Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Object injectedObject = null;
+        Class<?> type = findTypeImplementationFor(interfaceClazz);
+        if (!type.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException(interfaceClazz.getName()
+                    + ": this class haven't annotation 'Component'.");
+        }
+
+        Field[] fields = type.getDeclaredFields();
+        for (Field field: fields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Object fieldInstance = getInstance(field.getType());
+                injectedObject = createNewInstance(type);
+                field.setAccessible(true);
+                try {
+                    field.set(injectedObject, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can't inject the object "
+                            + fieldInstance.getClass().getName()
+                            + " to field " + field.getName(), e);
+                }
+            }
+        }
+        if (injectedObject == null) {
+            injectedObject = createNewInstance(type);
+        }
+        return injectedObject;
+    }
+
+    private Object createNewInstance(Class<?> type) {
+        if (instances.containsKey(type)) {
+            return instances.get(type);
+        }
+        try {
+            Object instance = type.getConstructor().newInstance();
+            instances.put(type, instance);
+            return instance;
+        } catch (InstantiationException | IllegalAccessException
+                 | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException("Can't create new instance of type: " + type.getName(), e);
+        }
+    }
+
+    private Class<?> findTypeImplementationFor(Class<?> interfaceClazz) {
+        Map<Class<?>, Class<?>> implementations = new HashMap<>();
+        implementations.put(FileReaderService.class, FileReaderServiceImpl.class);
+        implementations.put(ProductParser.class, ProductParserImpl.class);
+        implementations.put(ProductService.class, ProductServiceImpl.class);
+        return implementations.get(interfaceClazz);
     }
 }
