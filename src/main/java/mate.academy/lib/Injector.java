0494b748329a53e19c54a2c19a2a462e6f67cb46
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-08-28 16:27:14 +0300

    mistake with annotation was fixed

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,21 +1,21 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class Injector {
     private static final Injector injector = new Injector();
-    Map<Class<?>, Object> instances = new HashMap<>();
+    private Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
@@ -55,7 +55,8 @@ public class Injector {
             return instance;
         } catch (NoSuchMethodException | IllegalAccessException | InstantiationException
                  | InvocationTargetException e) {
-            throw new RuntimeException("Can't create a new instance of class: " + clazz.getName(), e);
+            throw new RuntimeException("Can't create a new instance of class: "
+                    + clazz.getName(), e);
         }
     }
 
