95719d75e2b479a8056025e4880303907f0d34e4
vadimkik2 <vadimkik@ukr.net>
2022-09-03 18:19:18 +0300

    Added my solution.

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -8,9 +8,8 @@ import mate.academy.service.ProductService;
 public class Main {
 
     public static void main(String[] args) {
-        // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
-        ProductService productService = null;
+        ProductService productService = (ProductService) Injector.getInstance(ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
