848bbebe7efb87ababe1ec46c4eb9eedad03c341
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-09-04 20:04:20 +0200

    add improvments

--- src/main/java/mate.academy/lib/Component.java
+++ src/main/java/mate.academy/lib/Component.java
@@ -5,8 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-    @Target(ElementType.TYPE)
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface Component {
-    }
-
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Component {
+}
