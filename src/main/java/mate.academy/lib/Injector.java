55f73b2f091bba5e3c2814e223ce58efc31b91c6
asha <ashajanebrown@gmail.com>
2022-09-05 13:33:36 +0300

    passed exception objects

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -36,7 +36,8 @@ public class Injector {
                     field.set(clazzImplementationInstance, fieldInstance);
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException("Can't initialize field value. "
-                            + "Class: " + clazz.getName() + ". Field: " + field.getName());
+                            + "Class: " + clazz.getName()
+                            + ". Field: " + field.getName(), e);
                 }
             }
         }
@@ -56,7 +57,8 @@ public class Injector {
             instances.put(clazz, instance);
             return instance;
         } catch (ReflectiveOperationException e) {
-            throw new RuntimeException("Can't create a new instance of " + clazz.getName());
+            throw new RuntimeException("Can't create a new instance of "
+                    + clazz.getName(), e);
         }
     }
 
