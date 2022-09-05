8363d4554b7485bb69c4295be2c21e6af478e8c8
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-05 16:06:59 +0300

    мабуть так

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -6,10 +6,8 @@ import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
-import mate.academy.service.ProductService;
 import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
-import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
@@ -64,9 +62,10 @@ public class Injector {
             return interfaceClazz;
         }
         Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
-        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
+//        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
         interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
         interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
-        return interfaceImplementations.get(interfaceClazz);
+        return interfaceImplementations.get(interfaceClazz) == null ? interfaceClazz
+                : interfaceImplementations.get(interfaceClazz);
     }
 }
