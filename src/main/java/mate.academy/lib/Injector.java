0f2c47918c9145107a642c725595586d6b384656
Vasyl Pankiv <wasilius2@gmail.com>
2022-09-05 12:00:27 +0300

    implement createNewInstance method

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,6 +1,8 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,7 +38,17 @@ public class Injector {
     }
 
     private Object createNewInstance(Class<?> clazz) {
-        return null;
+        if (instances.containsKey(clazz)) {
+            return instances.get(clazz);
+        }
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            Object instance = constructor.newInstance();
+            instances.put(clazz, instance);
+            return instance;
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("Can`t create a new instance of - " + clazz.getSimpleName());
+        }
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
