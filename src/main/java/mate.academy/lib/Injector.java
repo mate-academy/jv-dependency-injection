d0deb3d1468de3c4b15a337c17dbd17512c9f156
Thousandfold <you@example.com>
2022-09-05 09:58:26 +0300

    delete_annotatiions_from_interfaces_fix_checkstyle_exceptions

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -20,9 +20,9 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        if (interfaceClazz.isAnnotationPresent(Component.class)) {
             Object clazzImplementationInstance = null;
             Class<?> clazz = findImplementation(interfaceClazz);
+        if (clazz.isAnnotationPresent(Component.class)) {
             Field[] declaredFields = clazz.getDeclaredFields();
             for (Field field : declaredFields) {
                 if (field.isAnnotationPresent(Inject.class)) {
@@ -34,7 +34,7 @@ public class Injector {
                     } catch (IllegalAccessException e) {
                         throw new RuntimeException("Can't initialize field value! "
                                 + "Class:" + clazz.getName()
-                                + "Field: " + field.getName());
+                                + "Field: " + field.getName(), e);
                     }
                 }
             }
@@ -57,7 +57,7 @@ public class Injector {
             instances.put(clazz,instance);
             return instance;
         } catch (ReflectiveOperationException e) {
-            throw new RuntimeException("Can't create a new instance of" + clazz.getName());
+            throw new RuntimeException("Can't create a new instance of" + clazz.getName(), e);
         }
     }
 
