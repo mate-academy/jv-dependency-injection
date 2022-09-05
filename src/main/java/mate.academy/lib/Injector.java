a689dc4ed3086602b03442a4d6216b0eb8a72679
ArturZhadan <artur.zhadan.17@gmail.com>
2022-09-05 09:39:59 +0300

    fixed mistakes

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -27,8 +26,8 @@ public class Injector {
             throw new RuntimeException("There is no @Component annotation for the class: "
                     + clazz.getName());
         }
-        Field[] declaredFields = clazz.getDeclaredFields();
-        for (Field field : declaredFields) {
+        Field[] clazzDeclaredFields = clazz.getDeclaredFields();
+        for (Field field : clazzDeclaredFields) {
             if (field.isAnnotationPresent(Inject.class)) {
                 Object fieldInstance = getInstance(field.getType());
                 clazzImplementationInstance = createNewInstance(clazz);
@@ -56,8 +55,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (NoSuchMethodException | InstantiationException
-                 | IllegalAccessException | InvocationTargetException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can`t create a new instance of "
                     + clazz.getName());
         }
