94e24f41adb64e10ed4989af3e52bb9933638130
DooMLeoN <levberezov01@gmail.com>
2022-09-14 18:36:27 +0300

    added fixes: 1)add parent exception 2) make map interfaceAndImpl global;

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -14,8 +13,8 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
-    private final Map<Class<?>, Object> instances = new HashMap<>();
     private static final Map<Class<?>, Class<?>> interfaceAndImpl = new HashMap<>();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     static {
         interfaceAndImpl.put(FileReaderService.class, FileReaderServiceImpl.class);
