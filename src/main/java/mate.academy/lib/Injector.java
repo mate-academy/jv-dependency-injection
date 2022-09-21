7c27bce66607fab861895c5876efa22406549864
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-09-21 11:26:28 +0300

    created findImplementation method

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,40 @@
 package mate.academy.lib;
 
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private static Map<Class<?>,Object> mapInstances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
     }
 
-    public Object getInstance(Class<?> interfaceClazz) {
+    public static Object getInstance(Class<?> interfaceClazz) {
+        Class<?> clazz = findImplementation(interfaceClazz);
+        Object clazzImplementationInstance = null;
+        Field[] declaredFields = clazz.getDeclaredFields();
+
         return null;
+    }
+
+    private static Class<?> findImplementation(Class<?> interfaceClazz) {
+        Map<Class<?>,Class<?>> mapImplementations = new HashMap<>();
+        mapImplementations.put(ProductService.class, ProductServiceImpl.class);
+        mapImplementations.put(ProductParser.class, ProductParserImpl.class);
+        mapImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
+        if (interfaceClazz.isInterface()) {
+            return mapImplementations.get(interfaceClazz);
+        }
+        return interfaceClazz;
     }
 }
