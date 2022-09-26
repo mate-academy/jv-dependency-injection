2400f575b4fb2f30d00498a8ef7b34bcda5486e6
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-09-26 07:43:29 +0300

    created injector class

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -7,6 +7,7 @@ import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,8 +24,40 @@ public class Injector {
         Class<?> clazz = findImplementation(interfaceClazz);
         Object clazzImplementationInstance = null;
         Field[] declaredFields = clazz.getDeclaredFields();
+        if (!clazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Class " + clazz.getName() + " cannot be created");
+        }
+        for (Field field: declaredFields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Object fieldInstance = getInstance(field.getType());
+                clazzImplementationInstance = createInstrance(clazz);
+                try {
+                    field.setAccessible(true);
+                    field.set(clazzImplementationInstance,fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Cannot initialize field " + field.getName()
+                            + " for:" + clazz.getName());
+                }
+            }
+        }
+        if (clazzImplementationInstance == null) {
+            clazzImplementationInstance = createInstrance(clazz);
+        }
+        return clazzImplementationInstance;
+    }
 
-        return null;
+    private static Object createInstrance(Class<?> clazz) {
+        if (mapInstances.containsKey(clazz)) {
+            return mapInstances.get(clazz);
+        }
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            Object instance = constructor.newInstance();
+            mapInstances.put(clazz, instance);
+            return instance;
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(clazz.getName() + " cannot be created");
+        }
     }
 
     private static Class<?> findImplementation(Class<?> interfaceClazz) {
