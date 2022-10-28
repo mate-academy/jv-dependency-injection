6c6446fbc2e8d600099d4cbabea0276f1fc5d3e6
Yura Hryniv <superyura444@gmail.com>
2022-10-28 17:18:10 +0300

    Added ReflectiveOperationException v2

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -60,7 +60,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (ReflectiveOperationException  e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException();
         }
     }
