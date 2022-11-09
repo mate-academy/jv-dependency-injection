fe56660fbed43d95cb48d938651bcf9abdb8534f
thebitler <yurchyknikita@gmail.com>
2022-11-09 09:45:51 +0100

    Fixed imports order

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,12 +1,12 @@
 package mate.academy.lib;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 import mate.academy.service.impl.FileReaderServiceImpl;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
 
