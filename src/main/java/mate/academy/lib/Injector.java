25663e2b67ac07e000491f1eee57e1d1ebc3f807
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2022-12-09 23:04:48 +0200

    Injector was implemented

--- src/main/java/mate/academy/lib/Injector.java
+++ src/main/java/mate/academy/lib/Injector.java
@@ -6,8 +6,10 @@ import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
 import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
@@ -18,27 +20,25 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClass) {
-        if (!interfaceClass.isAnnotationPresent(Component.class)) {
-            throw new RuntimeException(
-                    "Injection failed, missing @Component annotaion on the class " + interfaceClass.getName());
-        }
         Class<?> inputClass = findImplementation(interfaceClass);
+        if (!inputClass.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException(
+                    "Injection failed, missing @Component annotaion " 
+            + "on the class " + interfaceClass.getName());
+        }
         Field[] declaredFields = inputClass.getDeclaredFields();
         Object inputClassImplementationInstance = null;
         if (interfaceClass.isAnnotationPresent(Component.class)) {
             for (Field field : declaredFields) {
                 if (field.isAnnotationPresent(Inject.class)) {
-                    // create a new object of field type if we have recursion dependencies 
                     Object fieldInstance = getInstance(field.getType());
-                    // create a new object or interface class or (implementation class)
                     inputClassImplementationInstance = createNewInstatnce(inputClass);
-                    // set 'field type object' to 'interfaceClass object'
                     try {
                         field.setAccessible(true);
                         field.set(inputClassImplementationInstance, fieldInstance);
                     } catch (IllegalAccessException e) {
-                        throw new RuntimeException("Can't initialize filed value. Class: " + inputClass.getName()
-                                + ", field: " + field.getName(), e);
+                        throw new RuntimeException("Can't initialize filed value. Class: " 
+                    + inputClass.getName() + ", field: " + field.getName(), e);
                     }
                 }
             }
@@ -51,18 +51,17 @@ public class Injector {
     }
 
     private Object createNewInstatnce(Class<?> inputClass) {
-        // if object already created - use it
         if (instances.containsKey(inputClass)) {
             return instances.get(inputClass);
         }
-        // create a new object
         try {
             Constructor<?> constructor = inputClass.getConstructor();
             Object newInstance = constructor.newInstance();
             instances.put(inputClass, newInstance);
             return newInstance;
         } catch (ReflectiveOperationException e) {
-            throw new RuntimeException("Can't create a new instance of " + inputClass.getName(), e);
+            throw new RuntimeException("Can't create a new instance of " 
+        + inputClass.getName(), e);
         }
     }
 
@@ -70,6 +69,7 @@ public class Injector {
         Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
         interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
         interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
+        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
         if (interfaceClass.isInterface()) {
             return interfaceImplementations.get(interfaceClass);
         }
