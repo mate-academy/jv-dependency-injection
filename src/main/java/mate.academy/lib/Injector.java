b7e79d6e3c278a1b1d7f187e3331c968a73860f7
Vitalina Volkivska <113260060+Vitalina-V@users.noreply.github.com>
2022-12-18 19:00:49 +0200

    the map was put on a class level the check for the presence of @Component annotation was done in the getInstance method many exceptions were replaced by the pattern

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -15,6 +14,7 @@ import mate.academy.service.impl.ProductServiceImpl;
 public class Injector {
     private static final Injector injector = new Injector();
     private Map<Class<?>, Object> instances = new HashMap<>();
+    private Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
 
     public static Injector getInjector() {
 
@@ -24,6 +24,11 @@ public class Injector {
     public Object getInstance(Class<?> interfaceClazz) {
         Object clazzImplementationInstance = null;
         Class<?> clazz = findImplementation(interfaceClazz);
+        if (!clazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Injection failed,"
+                    + " missing @Component annotaion on the class "
+                    + clazz.getName());
+        }
         Field[] declearedFIeld = interfaceClazz.getDeclaredFields();
         for (Field field: declearedFIeld) {
             if (field.isAnnotationPresent(Inject.class)) {
@@ -48,25 +53,17 @@ public class Injector {
         if (instances.containsKey(clazz)) {
             return instances.get(clazz);
         }
-        if (clazz.isAnnotationPresent(Component.class)) {
-            try {
-                Constructor<?> constructor = clazz.getConstructor();
-                Object insnance = constructor.newInstance();
-                instances.put(clazz, insnance);
-                return insnance;
-            } catch (NoSuchMethodException | InvocationTargetException
-                     | InstantiationException | IllegalAccessException e) {
-                throw new RuntimeException("Can`t create a new instance of " + clazz.getName());
-            }
-        } else {
-            throw new RuntimeException("Injection failed,"
-                    + " missing @Component annotaion on the class "
-                    + clazz.getName());
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            Object insnance = constructor.newInstance();
+            instances.put(clazz, insnance);
+            return insnance;
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Can`t create a new instance of " + clazz.getName());
         }
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
         interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
         interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
         interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
