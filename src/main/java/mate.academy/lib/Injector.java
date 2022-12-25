4845b97493b312d8b08d0e68523e7ab823595b14
Dmytro Busyhin <zza4etik@gmail.com>
2022-12-25 18:25:41 +0200

    map for interfaces implementations now fill at initialization

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -14,7 +14,9 @@ import mate.academy.service.impl.ProductServiceImpl;
 public class Injector {
     private static final Injector injector = new Injector();
     private Map<Class<?>, Object> instances = new HashMap<>();
-    private Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
+    private Map<Class<?>, Class<?>> interfaceImplementations = Map.of(FileReaderService.class,
+            FileReaderServiceImpl.class, ProductParser.class, ProductParserImpl.class,
+            ProductService.class, ProductServiceImpl.class);
 
     public static Injector getInjector() {
         return injector;
@@ -62,11 +64,6 @@ public class Injector {
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        if (interfaceImplementations.isEmpty()) {
-            interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
-            interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
-            interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
-        }
         if (interfaceClazz.isInterface()) {
             return interfaceImplementations.get(interfaceClazz);
         }
