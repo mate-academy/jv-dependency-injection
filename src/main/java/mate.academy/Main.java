aafea163c4c5f7b2567f7d3fe3ac24bf1fc7c946
Maksim Surzhik <kabaneri15@gmail.com>
2023-01-16 12:34:54 +0200

    cosmetic changes

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -6,11 +6,9 @@ import mate.academy.service.ProductService;
 public class Main {
 
     public static void main(String[] args) {
-
         Injector injector = Injector.getInjector();
         ProductService productService = (ProductService) injector.getInstance(ProductService.class);
-        productService
-                .getAllFromFile("products.txt")
+        productService.getAllFromFile("products.txt")
                 .forEach(System.out::println);
     }
 }
