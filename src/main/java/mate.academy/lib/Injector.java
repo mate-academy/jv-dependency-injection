09151932e5106ffb8a05803a9af60ada62c859e7
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-02-09 13:37:55 +0200

    feat: jv-dependency-injection #comment Injector implementation

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,65 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private static final Map<Class<?>, Class<?>> classImplementationMap = new HashMap<>();
+    private static final Map<Class<?>, Object> instanceMap = new HashMap<>();
+
+    static {
+        classImplementationMap.put(ProductService.class, ProductServiceImpl.class);
+        classImplementationMap.put(FileReaderService.class, FileReaderServiceImpl.class);
+        classImplementationMap.put(ProductParser.class, ProductParserImpl.class);
+    }
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> implementationClazz = findImplementation(interfaceClazz);
+        if (!implementationClazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Can not create " + implementationClazz.getName()
+                    + "class with Injector. Please consider using @Component");
+        }
+
+        if (instanceMap.containsKey(implementationClazz)) {
+            return instanceMap.get(implementationClazz);
+        }
+
+        try {
+            Object newInstance = implementationClazz.getConstructor().newInstance();
+            Field[] fields = implementationClazz.getDeclaredFields();
+            for (Field field: fields) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    Object fieldInstance = getInstance(field.getType());
+                    field.setAccessible(true);
+                    field.set(newInstance, fieldInstance);
+                }
+            }
+
+            return newInstance;
+        } catch (NoSuchMethodException | InvocationTargetException
+                    | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("Can not create: "
+                    + implementationClazz.getName() + " class.", e);
+        }
+    }
+
+    private Class<?> findImplementation(Class<?> clazz) {
+        if (clazz.isInterface()) {
+            return Injector.classImplementationMap.get(clazz);
+        }
+        return clazz;
     }
 }
