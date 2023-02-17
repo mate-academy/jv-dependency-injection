25e58d8f564384cbc56c3e1579706a7189e4ab14
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-02-17 21:36:51 +0200

    annotation created

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,5 +1,15 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
 
@@ -7,7 +17,33 @@ public class Injector {
         return injector;
     }
 
+    Map<Class<?>, Object> instances = new HashMap<>();
+
     public Object getInstance(Class<?> interfaceClazz) {
+        Class<?> clazz = findImplementation(interfaceClazz);
+        Field[] fields = interfaceClazz.getFields();
+        for (Field field: fields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Object instance = getInstance(field.getType());
+            }
+            if (interfaceClazz.isAnnotationPresent(Component.class)) {
+                Object clazzImplementationInstance = createNewInstance(clazz);
+            }
+        }
+
+
         return null;
+    }
+
+    private Object createNewInstance(Class<?> interfaceClazz) {
+        if (instances.containsKey())
+    }
+
+    public Class<?> findImplementation(Class<?> interfaceClazz) {
+        Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
+        interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
+        interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
+        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
+        return interfaceImplementations.get(interfaceClazz);
     }
 }
