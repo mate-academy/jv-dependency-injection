1e4b4620795129686dd67709711710f8633610f7
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-02-18 15:12:58 +0200

    injector logic created

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,6 +1,8 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -20,23 +22,34 @@ public class Injector {
     Map<Class<?>, Object> instances = new HashMap<>();
 
     public Object getInstance(Class<?> interfaceClazz) {
+        Object clazzImplementationInstance = null;
         Class<?> clazz = findImplementation(interfaceClazz);
-        Field[] fields = interfaceClazz.getFields();
-        for (Field field: fields) {
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
             if (field.isAnnotationPresent(Inject.class)) {
                 Object instance = getInstance(field.getType());
             }
             if (interfaceClazz.isAnnotationPresent(Component.class)) {
-                Object clazzImplementationInstance = createNewInstance(clazz);
+                clazzImplementationInstance = createNewInstance(clazz);
             }
         }
-
-
-        return null;
+        return clazzImplementationInstance;
     }
 
-    private Object createNewInstance(Class<?> interfaceClazz) {
-        if (instances.containsKey())
+    private Object createNewInstance(Class<?> clazz) {
+        if (instances.containsKey(clazz)) {
+            return instances.get(clazz);
+        }
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            Object instance = constructor.newInstance();
+            instances.put(clazz, instance);
+            return instance;
+        } catch (NoSuchMethodException | IllegalAccessException | InstantiationException |
+                 InvocationTargetException e) {
+            throw new RuntimeException("Can't create instance of the " +
+                    clazz.getName() + e);
+        }
     }
 
     public Class<?> findImplementation(Class<?> interfaceClazz) {
