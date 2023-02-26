97de59bce042260e85f07d5419af0049fe91c332
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2023-02-26 12:35:44 +0200

    fix code style

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,7 +1,6 @@
 package mate.academy.lib;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -72,9 +71,8 @@ public class Injector {
             Object instance = cls.getConstructor().newInstance();
             instancesMap.put(cls, instance);
             return instance;
-        } catch (InstantiationException | IllegalAccessException
-                 | InvocationTargetException | NoSuchMethodException e) {
-            throw new RuntimeException("Can't create new instance of " + cls.getName());
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Can't create new instance of " + cls.getName(), e);
         }
     }
 }
