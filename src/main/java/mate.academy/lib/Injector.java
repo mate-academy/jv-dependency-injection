adeb7706de88df62c87716cfcf20dae7a8d64f37
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2023-02-28 17:41:03 +0200

    fix solution according to comments

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -12,67 +12,73 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
-    private Map<Class<?>, Object> instancesMap = new HashMap<>();
+    private Map<Class<?>, Object> createdInstancesMap = new HashMap<>();
+    private Map<Class<?>, Class<?>> interfaceImplementationMap = createInterfaceImplementationMap();
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        Class<?> cls = getImpl(interfaceClazz);
-        if (!cls.isAnnotationPresent(Component.class)) {
+        Class<?> classImplementation = getImpl(interfaceClazz);
+        if (!classImplementation.isAnnotationPresent(Component.class)) {
             throw new RuntimeException("Injection failed, "
-                    + "missing @Component annotation on the class: " + cls.getName());
+                    + "missing @Component annotation on the class: "
+                    + classImplementation.getName());
         }
 
-        Object clsInstance = null;
-        Field[] declaredFields = cls.getDeclaredFields();
+        Object classInstance = null;
+        Field[] declaredFields = classImplementation.getDeclaredFields();
 
         for (Field field: declaredFields) {
             if (field.isAnnotationPresent(Inject.class)) {
                 // create service
                 Object serviceInstance = getInstance(field.getType());;
                 // create object
-                clsInstance = getPresentInstanceOrCreateNew(cls);
+                classInstance = getPresentInstanceOrCreateNew(classImplementation);
                 // set service as a field
                 try {
                     field.setAccessible(true);
-                    field.set(clsInstance, serviceInstance);
+                    field.set(classInstance, serviceInstance);
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException("Can't set field "
                             + field.getName()
-                            + " for " + clsInstance.getClass().getName() + " instance");
+                            + " for " + classInstance.getClass().getName() + " instance");
                 }
             }
         }
 
-        if (clsInstance == null) {
-            clsInstance = getPresentInstanceOrCreateNew(cls);
+        if (classInstance == null) {
+            classInstance = getPresentInstanceOrCreateNew(classImplementation);
         }
-        return clsInstance;
+        return classInstance;
     }
 
-    private Class<?> getImpl(Class<?> cls) {
-        Map<Class<?>, Class<?>> instances = new HashMap<>();
-        instances.put(FileReaderService.class, FileReaderServiceImpl.class);
-        instances.put(ProductParser.class, ProductParserImpl.class);
-        instances.put(ProductService.class, ProductServiceImpl.class);
-        if (cls.isInterface()) {
-            return instances.get(cls);
-        }
-        return cls;
+    private Map<Class<?>, Class<?>> createInterfaceImplementationMap() {
+        Map<Class<?>, Class<?>> map = new HashMap<>();
+        map.put(FileReaderService.class, FileReaderServiceImpl.class);
+        map.put(ProductParser.class, ProductParserImpl.class);
+        map.put(ProductService.class, ProductServiceImpl.class);
+        return map;
     }
 
-    private Object getPresentInstanceOrCreateNew(Class<?> cls) {
-        if (instancesMap.containsKey(cls)) {
-            return instancesMap.get(cls);
+    private Class<?> getImpl(Class<?> clazz) {
+        if (clazz.isInterface()) {
+            return interfaceImplementationMap.get(clazz);
+        }
+        return clazz;
+    }
+
+    private Object getPresentInstanceOrCreateNew(Class<?> clazz) {
+        if (createdInstancesMap.containsKey(clazz)) {
+            return createdInstancesMap.get(clazz);
         }
         try {
-            Object instance = cls.getConstructor().newInstance();
-            instancesMap.put(cls, instance);
+            Object instance = clazz.getConstructor().newInstance();
+            createdInstancesMap.put(clazz, instance);
             return instance;
         } catch (ReflectiveOperationException e) {
-            throw new RuntimeException("Can't create new instance of " + cls.getName(), e);
+            throw new RuntimeException("Can't create new instance of " + clazz.getName(), e);
         }
     }
 }
