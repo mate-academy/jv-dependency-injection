cd1a89e2d8201e76e383f282ea9661973f6f3e99
copypasterr <114933128+copypasterr@users.noreply.github.com>
2023-03-18 23:54:27 +0200

    style fix

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -10,7 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
         Injector injector = Injector.getInjector();
-        ProductService productService = (ProductService) injector.getInstance(ProductServiceImpl.class);
+        ProductService productService =
+                (ProductService) injector.getInstance(ProductServiceImpl.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
-
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
@@ -15,12 +14,11 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
     }
-
-    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     public Object getInstance(Class<?> interfaceClazz) {
         Object clazzImplementationInstance = null;
