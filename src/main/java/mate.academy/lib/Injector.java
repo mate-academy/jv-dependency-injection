4a2ce2ac1197a261dec5f72bcaddbd31fda8d5a0
KosMarch <kosmarchenko98@gmail.com>
2023-03-18 14:57:06 +0200

    simplified the catch block

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -52,8 +51,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (NoSuchMethodException | InvocationTargetException
-                 | InstantiationException | IllegalAccessException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create new instance of " + clazz.getName(), e);
         }
     }
@@ -64,7 +62,8 @@ public class Injector {
         interfaceImplementation.put(ProductParser.class, ProductParserImpl.class);
         interfaceImplementation.put(ProductService.class, ProductServiceImpl.class);
         if (!interfaceImplementation.get(interfaceClazz).isAnnotationPresent(Component.class)) {
-            throw new RuntimeException("This class has no annotation");
+            throw new RuntimeException("This class has no @Component annotation "
+                    + interfaceImplementation.get(interfaceClazz));
         }
         if (interfaceClazz.isInterface()) {
             return interfaceImplementation.get(interfaceClazz);
