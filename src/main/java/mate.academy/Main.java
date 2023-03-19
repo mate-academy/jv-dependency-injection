cfd8779c8939f3305a56fa2a029a589996596dd0
Mykyta Nikonets <nikita.nikonets@gmail.com>
2023-03-19 16:25:25 +0200

    implemented Injector

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -6,11 +6,10 @@ import mate.academy.model.Product;
 import mate.academy.service.ProductService;
 
 public class Main {
-
     public static void main(String[] args) {
         // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
-        ProductService productService = null;
+        ProductService productService = (ProductService)injector.getInstance(ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
