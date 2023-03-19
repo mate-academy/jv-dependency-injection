0b8225af5611592d79618d0c18c9fd61ba84dbb7
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2023-03-19 17:45:19 +0200

    renamed constructor

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -52,8 +52,8 @@ public class Injector {
             return instances.get(clazz);
         }
         try {
-            Constructor<?> constructor = clazz.getConstructor();
-            Object instance = constructor.newInstance();
+            Constructor<?> constructors = clazz.getConstructor();
+            Object instance = constructors.newInstance();
             instances.put(clazz, instance);
             return instance;
         } catch (ReflectiveOperationException e) {
