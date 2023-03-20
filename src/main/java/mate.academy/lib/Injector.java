1908ad77806252e6708b46e512ab6b0bbac32c36
Artem Grunin <artem.grunin.2006@gmail.com>
2023-03-20 09:28:47 +0200

    added Map of iterfaceImpl-s to class field

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -14,6 +14,7 @@ import mate.academy.service.impl.ProductServiceImpl;
 public class Injector {
     private static final Injector injector = new Injector();
     private final Map<Class<?>, Object> instances = new HashMap<>();
+    private Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
@@ -64,7 +65,7 @@ public class Injector {
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImplementations = Map.of(
+        interfaceImplementations = Map.of(
                 ProductService.class, ProductServiceImpl.class,
                 ProductParser.class, ProductParserImpl.class,
                 FileReaderService.class, FileReaderServiceImpl.class
