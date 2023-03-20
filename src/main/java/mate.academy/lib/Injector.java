7b172f891c3fea9ec8569176ae14e388745570a6
Viacheslav Kyslyi <kyslyiviacheslav@gmail.com>
2023-03-20 11:49:24 +0200

    fixed bugs rename methods, deleted redundant methods do Map implClazzez as class field

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,57 +2,53 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InaccessibleObjectException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
-import org.reflections.Reflections;
 
 public class Injector {
     private static final Injector injector = new Injector();
     private final Map<Class<?>, Object> instances = new HashMap<>();
+    private final Map<Class<?>, Class<?>> implClazzez = Map.of(
+            ProductService.class, ProductServiceImpl.class,
+            ProductParser.class, ProductParserImpl.class,
+            FileReaderService.class, FileReaderServiceImpl.class);
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        Class<?> clazz = findImplV2(interfaceClazz);
-        isImplClassWithAnotation(clazz);
+        Class<?> clazz = interfaceClazz.isInterface()
+                         ? implClazzez.get(interfaceClazz) : interfaceClazz;
+        if (!clazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Injection failed." + System.lineSeparator()
+                    + "Can't create instance of : " + clazz.getName() + System.lineSeparator()
+                    + "Because class haven't annotation: " + Component.class.getName());
+        }
         Object clazzImplInstance = null;
         Field[] clazzDeclaratedFields = clazz.getDeclaredFields();
         for (Field clazzField : clazzDeclaratedFields) {
             if (clazzField.isAnnotationPresent(Inject.class)) {
                 Object clazzFieldInstance = getInstance(clazzField.getType());
                 clazzImplInstance = createNewInstance(clazz);
-                clazzField.setAccessible(true);
                 try {
+                    clazzField.setAccessible(true);
                     clazzField.set(clazzImplInstance,clazzFieldInstance);
-                } catch (IllegalAccessException e) {
+                } catch (IllegalAccessException | InaccessibleObjectException e) {
                     throw new RuntimeException("Can't initialize field value"
                             + " Class: " + clazz.getName()
                             + " Field: " + clazzField.getName(),e);
                 }
             }
         }
-        if (clazzImplInstance == null) {
-            clazzImplInstance = createNewInstance(clazz);
-        }
-        return clazzImplInstance;
-    }
-
-    private boolean isImplClassWithAnotation(Class<?> clazz) {
-        if (!clazz.isAnnotationPresent(Component.class)) {
-            throw new RuntimeException("Injection failed." + System.lineSeparator()
-                    + "Can't create instance of : " + clazz.getName() + System.lineSeparator()
-                    + "Because class haven't annotation: " + Component.class.getName());
-        }
-        return true;
+        return clazzImplInstance == null ? createNewInstance(clazz) : clazzImplInstance;
     }
 
     private Object createNewInstance(Class<?> clazz) {
@@ -67,25 +63,5 @@ public class Injector {
         } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create instance of : " + clazz.getName(),e);
         }
-    }
-
-    private Class<?> findImpl(Class<?> clazz) {
-        Map<Class<?>, Class<?>> implClazzez = new HashMap<>();
-        implClazzez.put(ProductService.class, ProductServiceImpl.class);
-        implClazzez.put(ProductParser.class, ProductParserImpl.class);
-        implClazzez.put(FileReaderService.class, FileReaderServiceImpl.class);
-        if (clazz.isInterface()) {
-            return implClazzez.get(clazz);
-        }
-        return clazz;
-    }
-
-    private Class<?> findImplV2(Class<?> clazz) {
-        Reflections reflections = new Reflections("mate.academy");
-        Set<Class<?>> classes = (Set<Class<?>>) reflections.getSubTypesOf(clazz);
-        if (clazz.isInterface()) {
-            return classes.iterator().next();
-        }
-        return clazz;
     }
 }
