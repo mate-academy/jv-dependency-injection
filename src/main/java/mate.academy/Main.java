dad395f214b8a1af9d9e942e9138379ea7b097cb
potrap <potrapmax@gmail.com>
2023-03-20 12:38:50 +0200

    added Main class

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -1,17 +1,27 @@
 package mate.academy;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import mate.academy.lib.Injector;
-import mate.academy.model.Product;
+import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductService;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
-        ProductService productService = null;
-        List<Product> products = productService.getAllFromFile("products.txt");
-        products.forEach(System.out::println);
+
+        FileReaderService readerService = (FileReaderService) injector.getInstance(FileReaderService.class);
+        System.out.println(readerService.toString());
+
+        ProductService productService = (ProductService) injector.getInstance(ProductService.class);
+        System.out.println(productService.toString());
+        Field [] fields = productService.getClass().getDeclaredFields();
+        for(Field field : fields) {
+            System.out.println(field.getName());
+        }
+
+        Object actual = injector.getInstance(List.class);
+        System.out.println(actual.toString());
     }
 }
