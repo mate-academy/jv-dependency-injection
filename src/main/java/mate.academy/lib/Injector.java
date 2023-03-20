c2cc010db748f6b7737ca4585aefade18dde6339
De1eF <jatfun58@gmail.com>
2023-03-20 13:01:33 +0300

    Improved considering suggestions

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -14,6 +14,10 @@ import mate.academy.service.impl.ProductServiceImpl;
 public class Injector {
     private static final Injector injector = new Injector();
     private final Map<Class<?>, Object> instances = new HashMap<>();
+    private final  Map<Class<?>, Class<?>> interfaceImplementations = Map.of(
+        FileReaderService.class, FileReaderServiceImpl.class
+        ,ProductParser.class, ProductParserImpl.class
+        ,ProductService.class, ProductServiceImpl.class);
 
     public static Injector getInjector() {
         return injector;
@@ -44,7 +48,7 @@ public class Injector {
 
     private Object createNewInstance(Class<?> clazz) {
         if (!clazz.isAnnotationPresent(Component.class)) {
-            throw new RuntimeException("Trying to create a non component class: " + clazz);
+            throw new RuntimeException("Class: " + clazz + " does not contain @Component annotation");
         }
         if (instances.containsKey(clazz)) {
             return instances.get(clazz);
@@ -61,13 +65,9 @@ public class Injector {
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
-        interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
-        interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
-        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
         if (interfaceClazz.isInterface()) {
             return interfaceImplementations.get(interfaceClazz);
         }
-        throw new RuntimeException("Unsupported class: " + interfaceClazz);
+        return interfaceClazz;
     }
 }
