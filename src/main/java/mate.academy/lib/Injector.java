32fc4e87601b7e3de5e213c550cd6f3b904cf279
Denis Kalyakin <denis.kalyakin999@gmail.com>
2023-03-30 02:42:40 +0300

    Interface Implementations map was made a class field

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -14,6 +14,10 @@ import mate.academy.service.impl.ProductServiceImpl;
 public class Injector {
     private static final Injector injector = new Injector();
     private final Map<Class<?>, Object> instances = new HashMap<>();
+    private final Map<Class<?>, Class<?>> interfaceImplementation = Map.of(
+            ProductService.class, ProductServiceImpl.class,
+            ProductParser.class, ProductParserImpl.class,
+            FileReaderService.class, FileReaderServiceImpl.class);
 
     public static Injector getInjector() {
 
@@ -64,10 +68,6 @@ public class Injector {
     }
 
     private Class<?> findImplementation(Class<?> interfaceOrClazz) {
-        Map<Class<?>, Class<?>> interfaceImplementation = new HashMap<>();
-        interfaceImplementation.put(ProductService.class, ProductServiceImpl.class);
-        interfaceImplementation.put(ProductParser.class, ProductParserImpl.class);
-        interfaceImplementation.put(FileReaderService.class, FileReaderServiceImpl.class);
         if (interfaceOrClazz.isInterface()) {
             return interfaceImplementation.get(interfaceOrClazz);
         }
