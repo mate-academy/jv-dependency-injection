2d7b1713f40653c3b770a72829bb413b7343c7c0
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-03-30 18:00:29 +0300

    created two annotations "Component" and "Inject" and implemented in project

--- src/main/java/mate.academy/service/impl/ProductParserImpl.java
+++ src/main/java/mate.academy/service/impl/ProductParserImpl.java
@@ -1,20 +1,24 @@
 package mate.academy.service.impl;
 
 import java.math.BigDecimal;
+import mate.academy.lib.Component;
+import mate.academy.lib.Inject;
 import mate.academy.model.Product;
 import mate.academy.service.ProductParser;
 
+@Component
 public class ProductParserImpl implements ProductParser {
     public static final int ID_POSITION = 0;
     public static final int NAME_POSITION = 1;
     public static final int CATEGORY_POSITION = 2;
     public static final int DESCRIPTION_POSITION = 3;
     public static final int PRICE_POSITION = 4;
+    @Inject
+    private Product product;
 
     @Override
     public Product parse(String productInfo) {
         String[] data = productInfo.split(",");
-        Product product = new Product();
         product.setId(Long.valueOf(data[ID_POSITION]));
         product.setName(data[NAME_POSITION]);
         product.setCategory(data[CATEGORY_POSITION]);
