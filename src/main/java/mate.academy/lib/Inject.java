4ec0219537151915d1cfbffb6f190bdf2ea6ad00
Gamemod13 <igorfetov1997@gmail.com>
2023-05-26 01:05:25 +0300

    Implement injector used annotation - component and inject

--- src/main/java/mate.academy/lib/Inject.java
+++ src/main/java/mate.academy/lib/Inject.java
@@ -1,5 +1,11 @@
 package mate.academy.lib;
 
-public @interface Inject {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inject {
 }
