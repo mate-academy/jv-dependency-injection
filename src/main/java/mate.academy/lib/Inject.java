e9e386d4eaef51aa545721b2ed0792ea3fc1af55
Vladyslav Krutyus <kijdron@gmail.com>
2023-05-31 15:45:16 +0300

    fixed the project

--- src/main/java/mate.academy/lib/Inject.java
+++ src/main/java/mate.academy/lib/Inject.java
@@ -4,6 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Inject {
