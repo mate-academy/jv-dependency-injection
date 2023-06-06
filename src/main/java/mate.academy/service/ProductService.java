c21e594905c27cd580611ede2e551b5e0d615a7b
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-06-06 07:54:45 +0300

    add di solution

--- src/main/java/mate.academy/service/ProductService.java
+++ src/main/java/mate.academy/service/ProductService.java
@@ -1,7 +1,7 @@
 package mate.academy.service;
 
-import java.util.List;
 import mate.academy.model.Product;
+import java.util.List;
 
 public interface ProductService {
     List<Product> getAllFromFile(String filePath);
