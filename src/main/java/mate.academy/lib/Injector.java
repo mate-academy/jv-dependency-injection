00fa6f8a3f0c3817b4c2ec6bf8dd05f97bf48847
pochtalon <pochtalon1010@gmail.com>
2023-06-22 09:11:51 +0300

    Solution

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,24 +1,23 @@
 package mate.academy.lib;
 
-import mate.academy.service.FileReaderService;
-import mate.academy.service.ProductParser;
-import mate.academy.service.ProductService;
-import mate.academy.service.impl.FileReaderServiceImpl;
-import mate.academy.service.impl.ProductParserImpl;
-import mate.academy.service.impl.ProductServiceImpl;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
 
 public class Injector {
     private static final Injector injector = new Injector();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
     }
-    private Map<Class<?>, Object> instanses = new HashMap<>();
 
     public Object getInstance(Class<?> interfaceClazz) {
         Object clazzImplInstance = null;
@@ -43,12 +42,12 @@ public class Injector {
     }
 
     private Object createNewInstance(Class<?> clazz) {
-        if (instanses.containsKey(clazz)) {
-            return instanses.get(clazz);
+        if (instances.containsKey(clazz)) {
+            return instances.get(clazz);
         }
         try {
             Object instance = clazz.getConstructor().newInstance();
-            instanses.put(clazz, instance);
+            instances.put(clazz, instance);
             return instance;
         } catch (InstantiationException | IllegalAccessException
                  | InvocationTargetException | NoSuchMethodException e) {
