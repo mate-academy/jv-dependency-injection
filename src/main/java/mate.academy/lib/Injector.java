c390a3de12c974d7d5429f74538f97bd6776e849
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-07-01 03:41:31 +0300

    implemented Ñentire injector and tested solution

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,17 +1,14 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Injector {
     private static final Injector injector = new Injector();
@@ -23,6 +20,9 @@ public class Injector {
 
     public Object getInstance(Class<?> interfaceClazz) {
         Class<?> clazz = findImplementation(interfaceClazz);
+        if (!clazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Unsupported class! " + clazz);
+        }
         Object currentInstance = null;
         Field[] declaredFields = clazz.getDeclaredFields();
         for (Field field : declaredFields) {
@@ -66,7 +66,8 @@ public class Injector {
             existingInstances.put(clazz, newInstance);
             return newInstance;
         } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(String.format("Can't create new instance of %s class", clazz.getName()));
+            throw new RuntimeException(
+                    String.format("Can't create new instance of %s class", clazz.getName()));
         }
     }
 }
