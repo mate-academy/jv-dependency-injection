c4cb08b187cbd5b8ce5b5cd372531529a306885b
Zhuk Sergei <svzhuk@ukr.net>
2023-07-23 18:12:25 +0300

    Solution

--- src/main/java/mate.academy/lib/Inject.java
+++ src/main/java/mate.academy/lib/Inject.java
@@ -1,5 +1,10 @@
 package mate.academy.lib;
 
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Inject {
 
 }
