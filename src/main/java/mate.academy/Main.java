4a04fad95d36a99f5d698a7e8714bb629265afbc
Oleksandr Savenets <savenvoin@gmail.com>
2023-08-08 16:57:42 +0300

    remove redundant variable

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -10,8 +10,7 @@ public class Main {
     public static void main(String[] args) {
         // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
-        Object instance = injector.getInstance(ProductService.class);
-        ProductService productService = (ProductService) instance;
+        ProductService productService = (ProductService) injector.getInstance(ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
