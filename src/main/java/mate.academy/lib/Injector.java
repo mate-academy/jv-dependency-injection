3740db8e616dbf4fddda9fd75f8a01621e835190
Bogdan <97698639+don-bigdad@users.noreply.github.com>
2023-09-18 18:22:02 +0300

    injector implementation

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -15,11 +15,6 @@ public class Injector {
 
     private final Map<Class<?>, Object> instances = new HashMap<>();
 
-    private final Map<Class<?>, Class<?>> interfaceImpl = Map.of(
-            FileReaderService.class, FileReaderServiceImpl.class,
-            ProductParser.class, ProductParserImpl.class,
-            ProductService.class, ProductServiceImpl.class);
-
     public static Injector getInjector() {
         return injector;
     }
@@ -66,6 +61,10 @@ public class Injector {
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
+        Map<Class<?>, Class<?>> interfaceImpl = Map.of(
+                FileReaderService.class, FileReaderServiceImpl.class,
+                ProductParser.class, ProductParserImpl.class,
+                ProductService.class, ProductServiceImpl.class);
         if (interfaceClazz.isInterface()) {
             return interfaceImpl.get(interfaceClazz);
         }
