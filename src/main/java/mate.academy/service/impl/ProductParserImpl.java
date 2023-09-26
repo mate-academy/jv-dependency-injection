c8baec5767ffc97ec9989896d12da675eecee4e8
Anastasiia <anastasiiafi7@gmail.com>
2023-09-26 17:31:19 +0300

    solution added

--- src/main/java/mate.academy/service/impl/ProductParserImpl.java
+++ src/main/java/mate.academy/service/impl/ProductParserImpl.java
@@ -1,10 +1,14 @@
 package mate.academy.service.impl;
 
 import java.math.BigDecimal;
+import mate.academy.lib.Component;
 import mate.academy.model.Product;
 import mate.academy.service.ProductParser;
 
+@Component
+
 public class ProductParserImpl implements ProductParser {
+
     public static final int ID_POSITION = 0;
     public static final int NAME_POSITION = 1;
     public static final int CATEGORY_POSITION = 2;
