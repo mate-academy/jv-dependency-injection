248a68ca57991372597b6f68843a6657f2a34980
Vlad Kucher <kuchervlad49@gmail.com>
2023-10-13 12:45:32 +0300

    added injector impl, and annotations

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,55 @@
 package mate.academy.lib;
 
-public class Injector {
-    private static final Injector injector = new Injector();
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
-    public static Injector getInjector() {
-        return injector;
+public class Injector {
+    private final Map<Class<?>, Object> instances = new HashMap<>();
+
+    private Injector() {
+        registerImplementations();
+        injectDependencies();
     }
 
-    public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+    private void registerImplementations() {
+        instances.put(ProductService.class, new ProductServiceImpl());
+        instances.put(ProductParser.class, new ProductParserImpl());
+        instances.put(FileReaderService.class, new FileReaderServiceImpl());
+    }
+
+    private void injectDependencies() {
+        for (Object instance : instances.values()) {
+            for (Field field : instance.getClass().getDeclaredFields()) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    field.setAccessible(true);
+                    try {
+                        field.set(instance, instances.get(field.getType()));
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("Failed to inject dependency for field " + field.getName(), e);
+                    }
+                }
+            }
+        }
+    }
+
+    public <T> T getInstance(Class<T> type) {
+        Objects.requireNonNull(type, "Type cannot be null");
+        if (instances.containsKey(type)) {
+            return type.cast(instances.get(type));
+        } else {
+            throw new RuntimeException("No implementation found for type: " + type.getName());
+        }
+    }
+
+    public static Injector getInjector() {
+        return new Injector();
     }
 }
