df4af812bf61dc4eb5831171c27baf3dae09f4bb
User <Monsterhunter@ukr.net>
2023-10-17 01:42:06 +0300

    Implemented injector that used annotations in my task, did RuntimeException on getInstance if missing @Component annotation

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,34 @@
 package mate.academy.lib;
 
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private final Map<Class<?>, Object> instancesMap;
+
+    private Injector() {
+        instancesMap = new HashMap<>();
+        instancesMap.put(FileReaderService.class, new FileReaderServiceImpl());
+        instancesMap.put(ProductParser.class, new ProductParserImpl());
+        instancesMap.put(ProductService.class, new ProductServiceImpl());
+    }
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        if (!instancesMap.containsKey(interfaceClazz)) {
+            throw new RuntimeException("Injection failed, missing @Component annotation for class: "
+                    + interfaceClazz.getName());
+        }
+        return instancesMap.get(interfaceClazz);
     }
 }
