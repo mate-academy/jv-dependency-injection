7e0f6c2cb8328f9ec4cb2a348ad3efa81e6c487c
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-10-30 20:39:19 +0200

    added changes to the class ProductParserImpl

--- src/main/java/mate.academy/service/impl/ProductParserImpl.java
+++ src/main/java/mate.academy/service/impl/ProductParserImpl.java
@@ -3,7 +3,9 @@ package mate.academy.service.impl;
 import java.math.BigDecimal;
 import mate.academy.model.Product;
 import mate.academy.service.ProductParser;
+import mate.academy.lib.Component;
 
+@Component
 public class ProductParserImpl implements ProductParser {
     public static final int ID_POSITION = 0;
     public static final int NAME_POSITION = 1;
