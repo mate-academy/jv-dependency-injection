c3e22cb81272666f6fa044fabf17c2eada98da9b
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2023-11-07 16:23:53 +0200

    init commit. now it works

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -13,15 +12,16 @@ import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
-//    private static final String EXCEPTION_MESSAGE = "Unsupported class is passed";
-    private static HashMap<Class<?>, Class<?>> implementationMap = new HashMap<>();
+    private static final HashMap<Class<?>, Class<?>> implementationMap = new HashMap<>();
+
     static {
         implementationMap.put(FileReaderService.class, FileReaderServiceImpl.class);
         implementationMap.put(ProductParser.class, ProductParserImpl.class);
         implementationMap.put(ProductService.class, ProductServiceImpl.class);
     }
+
     private static final Injector injector = new Injector();
-    private Map<Class<?>, Class<?>> instances = new HashMap<>();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
@@ -51,11 +51,13 @@ public class Injector {
                 clazzImplementationInstance = createNewInstance(clazz);
             }
         } else {
-            throw new RuntimeException("Class " + clazz.getName() + " is missing @Component annotation.");
+            throw new RuntimeException("Class " + clazz.getName()
+                                                + " is missing @Component annotation.");
         }
 
         return clazzImplementationInstance;
     }
+
     private Object createNewInstance(Class<?> clazz) {
         if (instances.containsKey(clazz)) {
             return instances.get(clazz);
@@ -63,7 +65,7 @@ public class Injector {
         try {
             Constructor<?> constructor = clazz.getConstructor();
             Object instance = constructor.newInstance();
-            instances.put(clazz, instance.getClass());
+            instances.put(clazz, instance);
             return instance;
         } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create a new instance of " + clazz.getName());
