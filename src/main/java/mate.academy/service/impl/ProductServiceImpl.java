377fa43757dca7a3ec480544f73242f86ffd32bb
Nataliia <gzodchie@gmail.com>
2023-11-09 11:45:04 +0200

    added jv-dependency-injection solution

--- src/main/java/mate.academy/service/impl/ProductServiceImpl.java
+++ src/main/java/mate.academy/service/impl/ProductServiceImpl.java
@@ -2,17 +2,27 @@ package mate.academy.service.impl;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import mate.academy.lib.Component;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Injector;
 import mate.academy.model.Product;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 
+@Component
 public class ProductServiceImpl implements ProductService {
+    @Inject
     private ProductParser productParser;
+    @Inject
     private FileReaderService fileReaderService;
 
     @Override
     public List<Product> getAllFromFile(String filePath) {
+        Injector injector = Injector.getInjector();
+        ProductParser productParser = (ProductParser) injector.getInstance(ProductParser.class);
+        FileReaderService fileReaderService
+                = (FileReaderService) injector.getInstance(FileReaderService.class);
         return fileReaderService.readFile(filePath)
                 .stream()
                 .map(productParser::parse)
