74fa7ba9a161c2190322ce492f9fe085c8503624
IhorTrokhymchuk <hameleon009@gmail.com>
2023-11-13 11:15:21 +0200

    add injector and @inject

--- src/main/java/mate.academy/lib/Inject.java
+++ src/main/java/mate.academy/lib/Inject.java
@@ -1,5 +1,9 @@
 package mate.academy.lib;
 
-public @interface Inject {
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface  Inject {
 
 }
