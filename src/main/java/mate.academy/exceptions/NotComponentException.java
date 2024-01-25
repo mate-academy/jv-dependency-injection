515cbcfafd49709c1a4b1cb86937338343686f37
AdamMudrak <mudrets2000@gmail.com>
2024-01-25 14:58:21 +0200

    Add solution. I accidentally started working in main repo copy instead of a fork, so I had to copy the local result to a fork

---
+++ src/main/java/mate.academy/exceptions/NotComponentException.java
@@ -0,0 +1,7 @@
+package mate.academy.exceptions;
+
+public class NotComponentException extends RuntimeException {
+    public NotComponentException(String message) {
+        super(message);
+    }
+}
