3012efb380376f6a7a2411244165fc204f2fb220
Roman Soproniuk <151533079+RomanSoproniuk@users.noreply.github.com>
2024-01-31 11:25:09 +0200

    declare a variable outside of the method - change

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -14,6 +14,7 @@ import mate.academy.service.impl.ProductServiceImpl;
 public class Injector {
     private static final Injector injector = new Injector();
     private final Map<Class<?>, Object> instancesOfClazz = new HashMap<>();
+    private Map<Class<?>, Class<?>> interfaceImplementation;
 
     public static Injector getInjector() {
         return injector;
@@ -63,7 +64,7 @@ public class Injector {
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImplementation
+        interfaceImplementation
                 = Map.of(
                         ProductService.class, ProductServiceImpl.class,
                         ProductParser.class, ProductParserImpl.class,
