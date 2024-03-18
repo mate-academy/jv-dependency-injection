a55d486f25d23f8195c59f6e9e0c9fc810713fc6
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-03-18 14:46:52 +0200

    main version

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,57 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> clazz = findImplementation(interfaceClazz);
+        if (!instances.containsKey(clazz)) {
+            try {
+                Constructor<?> constructor = clazz.getConstructor();
+                Object instance = constructor.newInstance();
+                instances.put(clazz, instance);
+                for (Field field : clazz.getDeclaredFields()) {
+                    if (field.isAnnotationPresent(Inject.class)) {
+                        Object fieldInstance = getInstance(field.getType());
+                        field.setAccessible(true);
+                        field.set(instance, fieldInstance);
+                    }
+                }
+            } catch (NoSuchMethodException | IllegalAccessException
+                     | InstantiationException
+                     | InvocationTargetException e) {
+                throw new RuntimeException("Failed to instantiate class: " + clazz.getName(), e);
+            }
+        }
+        return instances.get(clazz);
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
+        interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
+        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
+        interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
+        if (interfaceClazz.isInterface()) {
+            return interfaceImplementations.get(interfaceClazz);
+        }
+        throw new RuntimeException("No implementation found for: "
+                + interfaceClazz.getName());
     }
 }
