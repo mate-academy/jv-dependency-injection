ed58c49c07db729084268cd972ab06ebfe49f934
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-03-19 20:10:44 +0200

    update version

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -14,7 +13,11 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
-    private Map<Class<?>, Object> instances = new HashMap<>();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
+    private final Map<Class<?>, Class<?>> interfaceImplementations = Map.of(
+            ProductParser.class, ProductParserImpl.class,
+            ProductService.class, ProductServiceImpl.class,
+            FileReaderService.class, FileReaderServiceImpl.class);
 
     public static Injector getInjector() {
         return injector;
@@ -24,6 +27,11 @@ public class Injector {
         Class<?> clazz = findImplementation(interfaceClazz);
         if (!instances.containsKey(clazz)) {
             try {
+                if (!clazz.isAnnotationPresent(Component.class)) {
+                    throw new RuntimeException("Class must have the Component annotation: "
+                            + clazz.getName());
+                }
+
                 Constructor<?> constructor = clazz.getConstructor();
                 Object instance = constructor.newInstance();
                 instances.put(clazz, instance);
@@ -34,9 +42,7 @@ public class Injector {
                         field.set(instance, fieldInstance);
                     }
                 }
-            } catch (NoSuchMethodException | IllegalAccessException
-                     | InstantiationException
-                     | InvocationTargetException e) {
+            } catch (ReflectiveOperationException e) {
                 throw new RuntimeException("Failed to instantiate class: " + clazz.getName(), e);
             }
         }
@@ -44,10 +50,6 @@ public class Injector {
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
-        interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
-        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
-        interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
         if (interfaceClazz.isInterface()) {
             return interfaceImplementations.get(interfaceClazz);
         }
