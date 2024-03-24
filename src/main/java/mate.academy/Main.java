0301d3d51c946c25d5e63f112acafd5c550edbca
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-03-24 15:27:13 +0200

    codestyle fix

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Injector injector = Injector.getInjector();
         ProductService productService = (ProductService) injector.getInstance(
-            ProductService.class);
+                ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,21 +1,21 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Injector {
     private static final Map<Class<?>, Class<?>> implementationMap =
-        Map.of(FileReaderService.class, FileReaderServiceImpl.class,
-            ProductParser.class, ProductParserImpl.class,
-            ProductService.class, ProductServiceImpl.class);
+            Map.of(FileReaderService.class, FileReaderServiceImpl.class,
+                ProductParser.class, ProductParserImpl.class,
+                ProductService.class, ProductServiceImpl.class);
 
     private static final Injector injector = new Injector();
     private final Map<Class<?>, Object> instances = new HashMap<>();
