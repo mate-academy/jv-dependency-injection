688c9c59a2459c5b1da1db47b1fce373a8cf2c58
azzilian <crusotdeath@gmail.com>
2024-03-26 00:07:46 +0200

    MVN fixes

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,17 +1,16 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Injector {
     private static final Injector injector = new Injector();
@@ -20,24 +19,21 @@ public class Injector {
             ProductParser.class, ProductParserImpl.class,
             ProductService.class, ProductServiceImpl.class);
 
+    private Map<Class<?>,Object> instances = new HashMap<>();
+
     public static Injector getInjector() {
         return injector;
     }
 
-    private Map<Class<?>,Object> instances = new HashMap<>();
-
     public Object getInstance(Class<?> interfaceClazz) {
-        Object clazzImplementationInstance = null;
         Class<?> clazz = findImplementation(interfaceClazz);
+        Object clazzImplementationInstance = null;
         checkClassComponentAnnotationIsPresent(clazz);
         Field[] declaredFields = interfaceClazz.getDeclaredFields();
         for (Field field: declaredFields) {
             if (field.isAnnotationPresent(Inject.class)) {
-
                 Object fieldInstance = getInstance(field.getType());
-
                 clazzImplementationInstance = createNewInstance(clazz);
-
                 try {
                     field.setAccessible(true);
                     field.set(clazzImplementationInstance, fieldInstance);
@@ -56,7 +52,7 @@ public class Injector {
     }
 
     private Object createNewInstance(Class<?> clazz) {
-        if (instances.containsKey(clazz)){
+        if (instances.containsKey(clazz)) {
             return instances.get(clazz);
         }
 
