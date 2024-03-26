84efe7b5c28677135a023aec6198209fdcf9887d
Taras Fedak <tarasfedak@me.com>
2024-03-26 13:02:47 +0200

    Final commit. Several improvements after student`s review.

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -14,7 +13,7 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
-    private static Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
+    private static final Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
 
     static {
         interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
@@ -22,7 +21,7 @@ public class Injector {
         interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
     }
 
-    private Map<Class<?>, Object> instances = new HashMap<>();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
@@ -31,15 +30,12 @@ public class Injector {
     public Object getInstance(Class<?> interfaceClazz) {
 
         Class<?> clazz = findImplementation(interfaceClazz);
-        Field[] declaredFields = interfaceClazz.getDeclaredFields();
+        Field[] declaredFields = clazz.getDeclaredFields();
         Object clazzImplementationInstance = null;
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(Inject.class)) {
-                //create a new object
                 Object fieldInstance = getInstance(field.getType());
-                //create interface
                 clazzImplementationInstance = createNewInstance(clazz);
-                //set field type to interface class
                 field.setAccessible(true);
                 try {
                     field.set(clazzImplementationInstance, fieldInstance);
@@ -55,10 +51,8 @@ public class Injector {
     }
 
     private Object createNewInstance(Class<?> clazz) {
-        if (clazz.isInterface() && !interfaceImplementations.containsKey(clazz)) {
-            throw new RuntimeException("can't create unsupported");
-        } else if (!interfaceImplementations.values().contains(clazz)) {
-            throw new RuntimeException("unsuported");
+        if (!interfaceImplementations.containsValue(clazz)) {
+            throw new RuntimeException("Unsupported");
         }
         if (instances.containsKey(clazz)) {
             return instances.get(clazz);
@@ -68,8 +62,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (InstantiationException | InvocationTargetException
-                 | IllegalAccessException | NoSuchMethodException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create new instance of " + clazz.getName());
         }
     }
