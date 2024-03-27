0db6c516f60f7c04e31300b105a2b32f5659a4ca
HAIOVIY <gaevoywork@gmail.com>
2024-03-27 15:10:08 +0200

    rewrote getInstance method, add Annotation @Target

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -3,13 +3,13 @@ package mate.academy;
 import java.util.List;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
-import mate.academy.service.impl.ProductServiceImpl;
+import mate.academy.service.ProductService;
 
 public class Main {
     public static void main(String[] args) {
         Injector injector = Injector.getInjector();
-        ProductServiceImpl productService = (ProductServiceImpl) injector.getInstance(
-                ProductServiceImpl.class);
+        ProductService productService = (ProductService) injector.getInstance(
+                ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -15,20 +14,25 @@ import mate.academy.service.impl.ProductServiceImpl;
 public class Injector {
     private static final Injector injector = new Injector();
     private final Map<Class<?>, Object> instaces = new HashMap<>();
+    private final Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
+
+    static {
+        injector.interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
+        injector.interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
+        injector.interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
+    }
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        Object clazzImplementationInstance = null;
         Class<?> clazz = findImplementation(interfaceClazz);
+        Object clazzImplementationInstance = createNewInstance(clazz);
         Field[] declaredFields = clazz.getDeclaredFields();
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(Inject.class)) {
                 Object fieldInstance = getInstance(field.getType());
-                clazzImplementationInstance = createNewInstance(clazz);
-
                 field.setAccessible(true);
                 try {
                     field.set(clazzImplementationInstance, fieldInstance);
@@ -37,9 +41,6 @@ public class Injector {
                             + "Class: " + clazz.getName() + "Field: " + field.getName());
                 }
             }
-        }
-        if (clazzImplementationInstance == null) {
-            clazzImplementationInstance = createNewInstance(clazz);
         }
         return clazzImplementationInstance;
     }
@@ -53,17 +54,12 @@ public class Injector {
             Object instance = constructor.newInstance();
             instaces.put(clazz, instance);
             return instance;
-        } catch (NoSuchMethodException | InstantiationException
-                 | IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException("Can't create a new instance of " + clazz.getName());
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Can't create a new instance of " + clazz.getName(), e);
         }
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
-        interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
-        interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
-        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
         if (interfaceClazz.isInterface()) {
             return interfaceImplementations.get(interfaceClazz);
         }
