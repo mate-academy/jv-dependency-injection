89e5b303a515ea403f6ed5a721de7b1905358458
Vladlen Soloviov <retkinf@gmail.com>
2024-04-10 19:18:11 +0300

    do HW don't understand why the main isn't working

--- src/main/java/mate.academy/service/impl/FileReaderServiceImpl.java
+++ src/main/java/mate.academy/service/impl/FileReaderServiceImpl.java
@@ -4,12 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import mate.academy.lib.Component;
 import mate.academy.service.FileReaderService;
 
+@Component
 public class FileReaderServiceImpl implements FileReaderService {
     @Override
     public List<String> readFile(String fileName) {
         try {
+            System.out.println(new File(fileName).toPath());
             return Files.readAllLines(new File(fileName).toPath());
         } catch (IOException e) {
             throw new RuntimeException("Can't read file: " + fileName, e);
