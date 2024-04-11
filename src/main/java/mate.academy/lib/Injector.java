fcc4d3dc0e593939f0b910f188b778e18e8babff
Vladlen Soloviov <retkinf@gmail.com>
2024-04-11 21:06:58 +0300

    new fixed

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -14,7 +14,12 @@ import mate.academy.service.impl.ProductServiceImpl;
 public class Injector {
     private static final Injector injector = new Injector();
     private Map<Class<?>, Object> instances = new HashMap<>();
-    private Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
+    private Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>(
+            Map.of(
+                    FileReaderService.class, FileReaderServiceImpl.class,
+                    ProductParser.class,ProductParserImpl.class,
+                    ProductService.class, ProductServiceImpl.class
+    ));
 
     public static Injector getInjector() {
         return injector;
@@ -62,12 +67,6 @@ public class Injector {
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        interfaceImplementations.putAll(
-                Map.of(
-                        FileReaderService.class, FileReaderServiceImpl.class,
-                        ProductParser.class,ProductParserImpl.class,
-                        ProductService.class, ProductServiceImpl.class
-        ));
         if (interfaceClazz.isInterface()) {
             return interfaceImplementations.get(interfaceClazz);
         }
