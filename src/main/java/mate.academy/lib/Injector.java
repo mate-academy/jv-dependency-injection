ac7d4cc5dd213e8b7211fcdb2fe8f9f991b7aa60
Anton <antchaycka@gmail.com>
2024-06-05 11:41:55 +0200

    made fixes according to review

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -45,7 +44,6 @@ public class Injector {
                     throw new RuntimeException("Can't initialize field value. Class: "
                             + clazz.getName() + ". Field: " + field.getName());
                 }
-
             }
         }
         if (clazzImplementationInstance == null) {
@@ -63,8 +61,7 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (InvocationTargetException | NoSuchMethodException
-                 | InstantiationException | IllegalAccessException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create a new instance of " + clazz.getName());
         }
     }
