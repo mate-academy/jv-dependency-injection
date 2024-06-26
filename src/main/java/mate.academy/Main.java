6f8e693ba1c127a88720db6e4f18ffeb70a6ae0d
mgsorigo <marek.gucma@sorigo.pl>
2024-06-26 21:11:07 +0200

    fix

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -1,14 +1,11 @@
 package mate.academy;
 
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
 import mate.academy.service.ProductService;
 
 public class Main {
-    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
         Injector injector = Injector.getInjector();
@@ -18,7 +15,7 @@ public class Main {
             List<Product> products = productService.getAllFromFile("products.txt");
             products.forEach(System.out::println);
         } catch (RuntimeException e) {
-            LOGGER.log(Level.SEVERE, "Runtime exception occurred: " + e.getMessage(), e);
+            System.err.println("Runtime exception occurred: " + e.getMessage());
         }
     }
 }
--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -4,8 +4,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
@@ -15,87 +13,66 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
-    private static final Logger LOGGER = Logger.getLogger(Injector.class.getName());
-    private final Map<Class<?>, Object> instances = new HashMap<>();
-    private final Map<Class<?>, Class<?>> interfaceImplementations = Map.of(
-            ProductService.class, ProductServiceImpl.class,
-            FileReaderService.class, FileReaderServiceImpl.class,
-            ProductParser.class, ProductParserImpl.class
-    );
+    private final Map<Class<?>, Class<?>> implementations;
+    private final Map<Class<?>, Object> instances;
+
+    private Injector() {
+        implementations = Map.of(
+                FileReaderService.class, FileReaderServiceImpl.class,
+                ProductParser.class, ProductParserImpl.class,
+                ProductService.class, ProductServiceImpl.class
+        );
+        instances = new HashMap<>();
+    }
 
     public static Injector getInjector() {
         return injector;
     }
 
-    public Object getInstance(Class<?> interfaceClass) {
-        try {
-            // Check if an instance already exists
-            if (instances.containsKey(interfaceClass)) {
-                return instances.get(interfaceClass);
-            }
+    public Object getInstance(Class<?> interfaceClazz) {
+        Class<?> clazz = getImplementation(interfaceClazz);
 
-            // Get the implementation class for the given interface
-            Class<?> implementationClass = interfaceImplementations.get(interfaceClass);
-            // Ensure the implementation class is annotated with @Component
-            if (implementationClass == null
-                    || !implementationClass.isAnnotationPresent(Component.class)) {
-                throw new IllegalArgumentException(
-                        "Injection failed, missing @Component annotation on the class "
-                                + (implementationClass != null
-                                ? implementationClass.getName() : "null")
-                );
-            }
-
-            // Create an instance of the implementation class
-            Object instance = createInstance(implementationClass);
-            // Store the instance in the map
-            instances.put(interfaceClass, instance);
-            // Inject dependencies into the instance
-            injectDependencies(instance);
-            return instance;
-        } catch (ReflectiveOperationException e) {
-            LOGGER.log(Level.SEVERE, "Injection failed for " + interfaceClass.getName()
-                    + " due to " + e.getClass().getSimpleName(), e);
-            throw new RuntimeException(
-                    "Injection failed for " + interfaceClass.getName()
-                            + " due to " + e.getClass().getSimpleName(), e
-            );
+        if (!clazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Missing @Component annotation in class: "
+                    + clazz.getName() + ". Injection failed!");
         }
-    }
 
-    private Object createInstance(Class<?> clazz) throws ReflectiveOperationException {
-        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
-        for (Constructor<?> constructor : constructors) {
-            if (constructor.isAnnotationPresent(Inject.class)) {
-                Class<?>[] parameterTypes = constructor.getParameterTypes();
-                Object[] parameters = new Object[parameterTypes.length];
-                for (int i = 0; i < parameterTypes.length; i++) {
-                    parameters[i] = getInstance(parameterTypes[i]);
-                }
-                return constructor.newInstance(parameters);
-            }
-        }
-        return clazz.getDeclaredConstructor().newInstance();
-    }
+        Object clazzImplementationInstance = createNewInstance(clazz);
 
-    // Method to inject dependencies into the fields of the instance
-    private void injectDependencies(Object instance) throws ReflectiveOperationException {
-        // Get all declared fields of the instance's class
-        Field[] fields = instance.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            // Check if the field is annotated with @Inject
+        for (Field field : clazz.getDeclaredFields()) {
             if (field.isAnnotationPresent(Inject.class)) {
-                // Save the current accessibility state of the field
-                boolean accessible = field.canAccess(instance);
-                // Make the field accessible to inject the dependency
-                field.setAccessible(true);
-                // Recursively get the instance for the field's type
                 Object fieldInstance = getInstance(field.getType());
-                // Inject the resolved dependency into the field
-                field.set(instance, fieldInstance);
-                // Restore the original accessibility state of the field
-                field.setAccessible(accessible);
+                field.setAccessible(true);
+                try {
+                    field.set(clazzImplementationInstance, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can't initialize field value. Class "
+                            + clazz.getName() + ". Field " + field.getName(), e);
+                }
             }
         }
+        return clazzImplementationInstance;
+    }
+
+    private Object createNewInstance(Class<?> clazz) {
+        if (instances.containsKey(clazz)) {
+            return instances.get(clazz);
+        }
+        Constructor<?> constructor;
+        try {
+            constructor = clazz.getConstructor();
+            Object newInstance = constructor.newInstance();
+            instances.put(clazz, newInstance);
+            return newInstance;
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Can't create a new instance of: " + clazz.getName(), e);
+        }
+    }
+
+    private Class<?> getImplementation(Class<?> interfaceClazz) {
+        if (interfaceClazz.isInterface()) {
+            return implementations.get(interfaceClazz);
+        }
+        return interfaceClazz;
     }
 }
