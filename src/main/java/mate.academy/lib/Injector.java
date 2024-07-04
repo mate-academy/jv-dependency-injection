4de0c96c2976a5be431b9131c04b55d2811bfef8
Jakub <jszymanski97@gmail.com>
2024-07-04 20:47:27 +0100

    New more general exception used in Injector. New class field created for the implementation's map to be created outside of method

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -15,6 +14,13 @@ import mate.academy.service.impl.ProductServiceImpl;
 public class Injector {
     private static final Injector injector = new Injector();
     private final Map<Class<?>, Object> instances = new HashMap<>();
+    private final Map<Class<?>, Class<?>> implementationsMap = new HashMap<>();
+
+    private Injector() {
+        implementationsMap.put(FileReaderService.class, FileReaderServiceImpl.class);
+        implementationsMap.put(ProductService.class, ProductServiceImpl.class);
+        implementationsMap.put(ProductParser.class, ProductParserImpl.class);
+    }
 
     public static Injector getInjector() {
         return injector;
@@ -60,17 +66,12 @@ public class Injector {
             Object object = constructor.newInstance();
             instances.put(clazz, object);
             return object;
-        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException
-                 | IllegalAccessException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create new instance of " + clazz.getName(),e);
         }
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> implementationsMap = new HashMap<>();
-        implementationsMap.put(FileReaderService.class, FileReaderServiceImpl.class);
-        implementationsMap.put(ProductService.class, ProductServiceImpl.class);
-        implementationsMap.put(ProductParser.class, ProductParserImpl.class);
         if (interfaceClazz.isInterface()) {
             if (implementationsMap.get(interfaceClazz) != null) {
                 return implementationsMap.get(interfaceClazz);
