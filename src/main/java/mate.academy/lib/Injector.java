4dc24c8b5ea52fc80aff41113b7b8b1de13810e3
Nikita Boyko <nikitabojko57@gmail.com>
2024-07-05 13:23:01 +0300

    change Injector.java file after AI mentor review

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -32,7 +31,8 @@ public class Injector {
                     field.setAccessible(true);
                     field.set(clazzImplementationInstance, fieldInstance);
                 } catch (IllegalAccessException e) {
-                    throw new RuntimeException("Can't set a value to object.");
+                    throw new RuntimeException("Can't set a value to field."
+                    + "Class: " + clazz.getName() + ". Field: " + field.getName());
                 }
             }
         }
@@ -44,7 +44,8 @@ public class Injector {
 
     private Object createNewInstance(Class<?> clazz) {
         if (!clazz.isAnnotationPresent(Component.class)) {
-            throw new RuntimeException("Can't create an instance.");
+            throw new RuntimeException("Can't create an instance for "
+                    + clazz.getName() + " class");
         }
         if (instances.containsKey(clazz)) {
             return instances.get(clazz);
@@ -54,9 +55,9 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException
-                 | InvocationTargetException e) {
-            throw new RuntimeException(e);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("There was an error creating a new instance of "
+            + clazz.getName() + " class.");
         }
     }
 
