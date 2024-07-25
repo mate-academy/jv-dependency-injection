488da0e09cf9235819d460fc47b5809ad2201258
foxstudent106239 <mykhailo.kornukh@gmail.com>
2024-07-25 19:19:22 +0300

    fix a few ai issues

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -51,18 +50,17 @@ public class Injector {
             Object newInstance = constructor.newInstance();
             instances.put(clazzImplementation, newInstance);
             return newInstance;
-        } catch (NoSuchMethodException | InstantiationException
-                 | InvocationTargetException | IllegalAccessException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create instance of class: "
                                                + clazzImplementation.getName());
         }
     }
 
     private Class<?> findImplementation(Class<?> clazz) {
-        Map<Class<?>, Class<?>> implementations = new HashMap<>();
-        implementations.put(FileReaderService.class, FileReaderServiceImpl.class);
-        implementations.put(ProductParser.class, ProductParserImpl.class);
-        implementations.put(ProductService.class, ProductServiceImpl.class);
+        Map<Class<?>, Class<?>> implementations = Map.of(FileReaderService.class, FileReaderServiceImpl.class,
+                                                         ProductParser.class, ProductParserImpl.class,
+                                                         ProductService.class, ProductServiceImpl.class
+        );
         if (clazz.isInterface()) {
             return implementations.get(clazz);
         }
