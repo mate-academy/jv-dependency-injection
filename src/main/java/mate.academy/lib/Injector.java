b83f707d6142b9989eb1a05240c2d639fbeb8144
foxstudent106239 <mykhailo.kornukh@gmail.com>
2024-07-25 17:35:01 +0300

    last

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,5 +1,7 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+
 public class Injector {
     private static final Injector injector = new Injector();
 
@@ -8,6 +10,14 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
+        Field[] fields = interfaceClazz.getFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                //create a new object of field type
+                //create an object of interface clazz, (or implementation class)
+                // set field type object to interface clazz object
+            }
+        }
         return null;
     }
 }
