81df26e75837fce69448df03fdc53ca6b6c45cf2
flamehost <flamehost@syneforge.com>
2024-08-07 22:36:48 +0300

    display edits

--- src/main/java/mate.academy/service/impl/ProductParserImpl.java
+++ src/main/java/mate.academy/service/impl/ProductParserImpl.java
@@ -1,7 +1,7 @@
 package mate.academy.service.impl;
 
-import mate.academy.lib.Component;
 import java.math.BigDecimal;
+import mate.academy.lib.Component;
 import mate.academy.model.Product;
 import mate.academy.service.ProductParser;
 
