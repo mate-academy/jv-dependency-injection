fe3152017ad87843f1ee9eafd3998d38e82931f0
smagles <lesialist@icloud.com>
2024-08-08 20:02:36 +0200

    Optimize Injector by storing interface-to-implementation mappings as a class field

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -14,7 +13,12 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
-    private Map<Class<?>, Object> instances = new HashMap<>();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
+    private final Map<Class<?>, Class<?>> implementationMap = Map.of(
+            ProductParser.class, ProductParserImpl.class,
+            ProductService.class, ProductServiceImpl.class,
+            FileReaderService.class, FileReaderServiceImpl.class
+    );
 
     public static Injector getInjector() {
         return injector;
@@ -58,19 +62,14 @@ public class Injector {
             instances.put(clazz, object);
             return object;
 
-        } catch (NoSuchMethodException | InstantiationException
-                 | InvocationTargetException | IllegalAccessException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create a new instance of " + clazz.getName(), e);
         }
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> implemetationMap = new HashMap<>();
-        implemetationMap.put(ProductParser.class, ProductParserImpl.class);
-        implemetationMap.put(ProductService.class, ProductServiceImpl.class);
-        implemetationMap.put(FileReaderService.class, FileReaderServiceImpl.class);
         if (interfaceClazz.isInterface()) {
-            return implemetationMap.get(interfaceClazz);
+            return implementationMap.get(interfaceClazz);
         }
         return interfaceClazz;
     }
