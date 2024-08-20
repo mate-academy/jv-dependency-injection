e796470aa4717bfa78cd154ff79c0913d3f83243
OD <sdenisenko055@gmail.com>
2024-08-20 12:33:37 +0300

    @Inject from Main was removed by getInstance method Fixed @Target in Inject annotation

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -1,7 +1,6 @@
 package mate.academy;
 
 import java.util.List;
-import mate.academy.lib.Inject;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
 import mate.academy.service.ProductService;
@@ -11,8 +10,7 @@ public class Main {
     public static void main(String[] args) {
         // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
-        @Inject
-        ProductService productService = null;
+        ProductService productService = (ProductService) injector.getInstance(ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
