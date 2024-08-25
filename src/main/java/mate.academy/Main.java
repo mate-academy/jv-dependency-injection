d1437a418bcd2ee113db4b1064368f6a0ec7a0fa
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-08-25 15:26:39 +0200

    ProductServiceImpl replaced with an interface inside of the getInstance method call

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -3,6 +3,7 @@ package mate.academy;
 import java.util.List;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
+import mate.academy.service.ProductService;
 import mate.academy.service.impl.ProductServiceImpl;
 
 public class Main {
@@ -11,7 +12,7 @@ public class Main {
         // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
         ProductServiceImpl productService = (ProductServiceImpl) injector
-                .getInstance(ProductServiceImpl.class);
+                .getInstance(ProductService.class);
         List<Product> products = productService.getAllFromFile("products.txt");
         products.forEach(System.out::println);
     }
