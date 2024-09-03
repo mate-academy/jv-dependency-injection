7098a7cc57ec181140e461a09e31ad89855cdf12
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-03 11:16:09 +0300

    ProductServiceImpl.java

--- src/main/java/mate.academy/service/impl/ProductServiceImpl.java
+++ src/main/java/mate.academy/service/impl/ProductServiceImpl.java
@@ -2,9 +2,12 @@ package mate.academy.service.impl;
 
 import mate.academy.lib.Component;
 import mate.academy.lib.Inject;
+import mate.academy.model.Product;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 import mate.academy.service.FileReaderService;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ProductServiceImpl implements ProductService {
@@ -14,8 +17,8 @@ public class ProductServiceImpl implements ProductService {
     private FileReaderService fileReaderService;
 
     @Override
-    public List<Product> getAllFromFile(String filePath) {
-        return fileReaderService.readFile(filePath)
+    public List<Product> getAllProducts() {
+        return fileReaderService.readFile("products.txt")
                 .stream()
                 .map(productParser::parse)
                 .collect(Collectors.toList());
