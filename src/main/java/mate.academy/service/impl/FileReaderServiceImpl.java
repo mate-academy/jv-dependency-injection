d6dc2c8da68c3f7d0e4473aec4ab5573feb2e125
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-03 11:12:25 +0300

    FileReaderServiceImpl.java

--- src/main/java/mate.academy/service/impl/FileReaderServiceImpl.java
+++ src/main/java/mate.academy/service/impl/FileReaderServiceImpl.java
@@ -1,11 +1,9 @@
 package mate.academy.service.impl;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
+import mate.academy.lib.Component;
 import mate.academy.service.FileReaderService;
 
+@Component
 public class FileReaderServiceImpl implements FileReaderService {
     @Override
     public List<String> readFile(String fileName) {
