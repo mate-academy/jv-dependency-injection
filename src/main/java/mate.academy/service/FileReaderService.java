e8b95d3a0b79cefb15c7fc34fd58e45b7b456536
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-03 11:13:44 +0300

    FileReaderService.java

--- src/main/java/mate.academy/service/FileReaderService.java
+++ src/main/java/mate.academy/service/FileReaderService.java
@@ -1,7 +1,8 @@
 package mate.academy.service;
 
-import java.util.List;
+import mate.academy.lib.Component;
 
+@Component
 public interface FileReaderService {
     List<String> readFile(String fileName);
 }
