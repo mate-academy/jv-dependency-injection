5e7ab0979be7d6e3731f22e62a16854315b72880
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-03 11:16:31 +0300

    ProductParserImpl.java

--- src/main/java/mate.academy/service/impl/ProductParserImpl.java
+++ src/main/java/mate.academy/service/impl/ProductParserImpl.java
@@ -1,25 +1,18 @@
 package mate.academy.service.impl;
 
 import mate.academy.lib.Component;
+import mate.academy.model.Product;
 import mate.academy.service.ProductParser;
+import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 public class ProductParserImpl implements ProductParser {
-    public static final int ID_POSITION = 0;
-    public static final int NAME_POSITION = 1;
-    public static final int CATEGORY_POSITION = 2;
-    public static final int DESCRIPTION_POSITION = 3;
-    public static final int PRICE_POSITION = 4;
-
     @Override
-    public Product parse(String productInfo) {
-        String[] data = productInfo.split(",");
-        Product product = new Product();
-        product.setId(Long.valueOf(data[ID_POSITION]));
-        product.setName(data[NAME_POSITION]);
-        product.setCategory(data[CATEGORY_POSITION]);
-        product.setDescription(data[DESCRIPTION_POSITION]);
-        product.setPrice(new BigDecimal(data[PRICE_POSITION]));
-        return product;
+    public Product parse(String line) {
+        String[] parts = line.split(",");
+        String name = parts[0];
+        BigDecimal price = new BigDecimal(parts[1]);
+        return new Product(name, price);
     }
 }
