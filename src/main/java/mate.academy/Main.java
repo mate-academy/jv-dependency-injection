99310cc880958dbbcf2cb000d9e12bf8f2057643
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-04 13:38:36 +0300

    Main.java

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -1,15 +1,12 @@
 package mate.academy;
 
-import java.util.List;
 import mate.academy.lib.Injector;
-import mate.academy.model.Product;
 import mate.academy.service.ProductService;
 
 public class Main {
     public static void main(String[] args) {
         Injector injector = Injector.getInjector();
-        ProductService productService = (ProductService) injector.getInstance(ProductService.class);
-        List<Product> products = productService.getAllFromFile("products.txt");
-        products.forEach(System.out::println);
+        ProductService productService = injector.getInstance(ProductService.class);
+        productService.getAllProducts().forEach(System.out::println);
     }
 }
