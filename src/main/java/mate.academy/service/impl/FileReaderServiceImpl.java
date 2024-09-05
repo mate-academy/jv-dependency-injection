981fa136a5e5c4d6c18c8c337bf6571ba8b9970a
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-05 07:37:10 +0300

    FileReaderServiceImpl.java

--- src/main/java/mate.academy/service/impl/FileReaderServiceImpl.java
+++ src/main/java/mate.academy/service/impl/FileReaderServiceImpl.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
-import java.util.stream.Collectors;
 import mate.academy.lib.Component;
 import mate.academy.service.FileReaderService;
 
@@ -13,10 +12,9 @@ public class FileReaderServiceImpl implements FileReaderService {
     @Override
     public List<String> readFile(String fileName) {
         try {
-            return Files.lines(new File(fileName).toPath())
-                    .collect(Collectors.toList());
+            return Files.readAllLines(new File(fileName).toPath());
         } catch (IOException e) {
-            throw new RuntimeException("Can't read file " + fileName, e);
+            throw new RuntimeException("Can't read file: " + fileName, e);
         }
     }
 }
