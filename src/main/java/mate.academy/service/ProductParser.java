ee629d1f1ea07fe8aa1c3d7f6dd2d0f3056e11a8
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-09-05 07:36:29 +0300

    ProductParser.java

--- src/main/java/mate.academy/service/ProductParser.java
+++ src/main/java/mate.academy/service/ProductParser.java
@@ -3,5 +3,11 @@ package mate.academy.service;
 import mate.academy.model.Product;
 
 public interface ProductParser {
-    Product parse(String line);
+    /**
+     * Parses a product from a string.
+     *
+     * @param productInfo the string containing product information
+     * @return newly create {@link Product} object based on the `productInfo` input.
+     */
+    Product parse(String productInfo);
 }
