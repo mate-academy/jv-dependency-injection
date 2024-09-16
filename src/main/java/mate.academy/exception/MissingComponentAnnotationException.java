d91dab5d4e8d5ee4522743c82fb884edcea19a4f
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-09-16 20:15:15 +0300

    created custom exception

---
+++ src/main/java/mate.academy/exception/MissingComponentAnnotationException.java
@@ -0,0 +1,7 @@
+package mate.academy.exception;
+
+public class MissingComponentAnnotationException extends RuntimeException {
+    public MissingComponentAnnotationException(String message) {
+        super(message);
+    }
+}
