5a66b1b13ba39af3f61e2b12c21ca949bdb0696a
Igor Neminuschii <igorgorslav@gmail.com>
2024-10-23 18:59:30 +0300

    implemented Injector class

---
+++ src/main/java/mate.academy/exception/RequiredAnnotationAbsenceException.java
@@ -0,0 +1,7 @@
+package mate.academy.exception;
+
+public class RequiredAnnotationAbsenceException extends RuntimeException {
+    public RequiredAnnotationAbsenceException(String message) {
+        super(message);
+    }
+}
