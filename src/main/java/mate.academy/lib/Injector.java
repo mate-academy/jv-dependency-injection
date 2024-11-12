9187f0ce3025e2d94b91414f789791e185f74fe3
homeworkfirst <zookotelif@gmail.com>
2024-11-12 20:19:11 +0200

    Implement dependency injection with @Component and @Inject annotations, and provide service implementations for FileReaderService, ProductParser, and ProductService.

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,69 @@
 package mate.academy.lib;
 
+import mate.academy.service.ProductService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.impl.ProductServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private Map<Class<?>, Object> instances = new HashMap<>();
+
+    private Injector() {}
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> implClass = findImplementation(interfaceClazz);
+        if (!implClass.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Class " + implClass.getName() + " is not marked with @Component");
+        }
+
+        // Если экземпляр уже существует, возвращаем его
+        if (instances.containsKey(implClass)) {
+            return instances.get(implClass);
+        }
+
+        // Создаем новый экземпляр
+        try {
+            Constructor<?> constructor = implClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            Object instance = constructor.newInstance();
+
+            // Инжектим зависимости
+            for (Field field : implClass.getDeclaredFields()) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    Object dependency = getInstance(field.getType());
+                    field.setAccessible(true);
+                    field.set(instance, dependency);
+                }
+            }
+
+            instances.put(implClass, instance);
+            return instance;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create instance of " + implClass.getName(), e);
+        }
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        if (interfaceClazz == ProductService.class) {
+            return ProductServiceImpl.class;
+        }
+        if (interfaceClazz == ProductParser.class) {
+            return ProductParserImpl.class;
+        }
+        if (interfaceClazz == FileReaderService.class) {
+            return FileReaderServiceImpl.class;
+        }
+        throw new RuntimeException("No implementation found for " + interfaceClazz.getName());
     }
 }
--- src/main/java/mate.academy/model/Product.java
+++ src/main/java/mate.academy/model/Product.java
@@ -9,6 +9,7 @@ public class Product {
     private String description;
     private BigDecimal price;
 
+    // Геттеры и сеттеры
     public Long getId() {
         return id;
     }
@@ -51,12 +52,12 @@ public class Product {
 
     @Override
     public String toString() {
-        return "Product{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", category='" + category + '\''
-                + ", description='" + description + '\''
-                + ", price=" + price
-                + '}';
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
