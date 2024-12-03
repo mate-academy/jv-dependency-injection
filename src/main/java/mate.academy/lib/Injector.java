8cca9288e350fa938f0552b25f83a42e58fc89fe
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-12-03 09:19:41 +0100

    final

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -14,6 +14,7 @@ import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
+
     private static final Injector injector = new Injector();
     private Map<Class<?>, Object> instances = new HashMap<>();
 
