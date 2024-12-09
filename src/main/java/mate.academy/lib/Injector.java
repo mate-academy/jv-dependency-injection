9d6203d4ca85c0897a4fe36e7e8882483a2aa598
YanHrdMkr <yanstabrovski3@gmail.com>
2024-12-09 23:44:36 +0200

    Second try

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,12 +1,18 @@
 package mate.academy.lib;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Injector {
     private static final Injector injector = new Injector();
-    private final Map<Class<?>, Object> instances = new HashMap<>();
+
+    private static final Map<Class<?>, Class<?>> IMPLEMENTATIONS = Map.of(
+            mate.academy.service.ProductService.class,
+            mate.academy.service.impl.ProductServiceImpl.class,
+            mate.academy.service.ProductParser.class,
+            mate.academy.service.impl.ProductParserImpl.class,
+            mate.academy.service.FileReaderService.class,
+            mate.academy.service.impl.FileReaderServiceImpl.class
+    );
 
     private Injector() {
     }
@@ -17,47 +23,29 @@ public class Injector {
 
     public Object getInstance(Class<?> interfaceClazz) {
         try {
-            if (instances.containsKey(interfaceClazz)) {
-                return instances.get(interfaceClazz);
+            if (!IMPLEMENTATIONS.containsKey(interfaceClazz)) {
+                throw new RuntimeException("No implementation found for interface: "
+                        + interfaceClazz.getName());
             }
 
-            Class<?> implClazz = findImplementation(interfaceClazz);
+            Class<?> implClazz = IMPLEMENTATIONS.get(interfaceClazz);
             if (!implClazz.isAnnotationPresent(Component.class)) {
                 throw new RuntimeException("Class " + implClazz.getName()
                         + " is not annotated with @Component");
             }
 
             Object implInstance = implClazz.getDeclaredConstructor().newInstance();
-            Field[] fields = implClazz.getDeclaredFields();
-
-            for (Field field : fields) {
+            for (var field : implClazz.getDeclaredFields()) {
                 if (field.isAnnotationPresent(Inject.class)) {
-                    Class<?> fieldType = field.getType();
-                    Object fieldInstance = getInstance(fieldType);
+                    Object dependency = getInstance(field.getType());
                     field.setAccessible(true);
-                    field.set(implInstance, fieldInstance);
+                    field.set(implInstance, dependency);
                 }
             }
-
-            instances.put(interfaceClazz, implInstance);
             return implInstance;
         } catch (Exception e) {
             throw new RuntimeException("Can't create instance of class: "
                     + interfaceClazz.getName(), e);
         }
-    }
-
-    private Class<?> findImplementation(Class<?> interfaceClazz) {
-        if (interfaceClazz.isInterface()) {
-            if (interfaceClazz.equals(mate.academy.service.ProductService.class)) {
-                return mate.academy.service.impl.ProductServiceImpl.class;
-            } else if (interfaceClazz.equals(mate.academy.service.ProductParser.class)) {
-                return mate.academy.service.impl.ProductParserImpl.class;
-            } else if (interfaceClazz.equals(mate.academy.service.FileReaderService.class)) {
-                return mate.academy.service.impl.FileReaderServiceImpl.class;
-            }
-        }
-        throw new RuntimeException("No implementation found for interface: "
-                + interfaceClazz.getName());
     }
 }
