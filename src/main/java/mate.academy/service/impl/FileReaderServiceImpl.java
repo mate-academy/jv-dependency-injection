08ce7a951b129631ad86274100f330ca7ad3362b
Yaroslav Narozhnyi <gnredit9@gmail.com>
2025-01-03 02:09:14 +0200

    finalistic

--- src/main/java/mate.academy/service/impl/FileReaderServiceImpl.java
+++ src/main/java/mate.academy/service/impl/FileReaderServiceImpl.java
@@ -1,11 +1,11 @@
 package mate.academy.service.impl;
 
-import mate.academy.lib.Component;
-import mate.academy.lib.Inject;
 import java.util.List;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import mate.academy.lib.Component;
+import mate.academy.lib.Inject;
 import mate.academy.service.FileReaderService;
 
 @Component
