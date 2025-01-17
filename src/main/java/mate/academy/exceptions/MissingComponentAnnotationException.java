afd973b5b9d5d26b9c2e208c523356b5260ce340
Loiev Roman <robolito0420@gmail.com>
2025-01-17 02:12:21 +0200

    Task is done.

---
+++ src/main/java/mate/academy/exceptions/MissingComponentAnnotationException.java
@@ -0,0 +1,7 @@
+package mate.academy.exceptions;
+
+public class MissingComponentAnnotationException extends RuntimeException {
+    public MissingComponentAnnotationException(String message) {
+        super(message);
+    }
+}
