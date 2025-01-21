f1bd2191405dc4f8b5669fe815a82cd971d764cb
Kate Kraska <kate2002.kk42@gmail.com>
2025-01-21 19:40:41 +0200

    Initial commit for hw-solution2

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,15 +1,15 @@
 package mate.academy.lib;
 
-import mate.academy.service.FileReaderService;
-import mate.academy.service.ProductParser;
-import mate.academy.service.ProductService;
-import mate.academy.service.impl.FileReaderServiceImpl;
-import mate.academy.service.impl.ProductParserImpl;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
@@ -19,7 +19,7 @@ public class Injector {
         return injector;
     }
 
-    private final Map<Class<?>, Object> instances = new HashMap<>();
+    private Map<Class<?>, Object> instances = new HashMap<>();
 
     public Object getInstance(Class<?> interfaceClazz) {
         Object clazzImplementationInstance = null;
