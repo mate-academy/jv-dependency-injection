099166001ebb7a5d67d2fb8e1b4f1bf6aebd6456
Bohdan_SSX <mr.dimov77@gmail.com>
2025-02-07 22:01:54 +0200

    More main checks added

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -6,28 +6,38 @@ import mate.academy.model.Product;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
+import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
         Injector injector = Injector.getInjector();
-        ProductService productService
+        ProductService productServiceImpl
                 = (ProductService) injector.getInstance(ProductServiceImpl.class);
-        List<Product> products = productService.getAllFromFile("products.txt");
+        List<Product> products = productServiceImpl.getAllFromFile("products.txt");
         products.forEach(System.out::println);
 
-        ProductService productService2
+        ProductService productService
                 = (ProductService) injector.getInstance(ProductService.class);
-        List<Product> products2 = productService2.getAllFromFile("products.txt");
+        List<Product> products2 = productService.getAllFromFile("products.txt");
         products2.forEach(System.out::println);
 
         ProductParser productParser = (ProductParser) injector.getInstance(ProductParser.class);
         productParser.parse("12,Iphone 18,phones,amazing,1999");
 
+        ProductParser productParserImpl
+                = (ProductParser) injector.getInstance(ProductParserImpl.class);
+        productParserImpl.parse("33,Iphone 28,phones,Too cheap,4999");
+
         FileReaderService fileReaderService
                 = (FileReaderService) injector.getInstance(FileReaderService.class);
         System.out.println(fileReaderService.readFile("products.txt"));
 
+        FileReaderService fileReaderServiceImpl
+                = (FileReaderService) injector.getInstance(FileReaderService.class);
+        System.out.println(fileReaderServiceImpl.readFile("products.txt"));
+
+        String incorrectObject = (String) injector.getInstance(String.class);
     }
 }
