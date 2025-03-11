ecc214bdd8f372be69c97146cb840d2e2c3c2aad
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-03-11 04:05:14 +0100

    solution

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,73 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Injector {
     private static final Injector injector = new Injector();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
+    private final Map<Class<?>, Class<?>> interfaceImplementations = Map.of(
+            mate.academy.service.ProductParser.class,
+            mate.academy.service.impl.ProductParserImpl.class,
+            mate.academy.service.FileReaderService.class,
+            mate.academy.service.impl.FileReaderServiceImpl.class,
+            mate.academy.service.ProductService.class,
+            mate.academy.service.impl.ProductServiceImpl.class
+    );
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        if (!interfaceClazz.isInterface()) {
+            throw new RuntimeException("Only interfaces can be passed to the Injector");
+        }
+
+        Class<?> implementationClazz = findImplementation(interfaceClazz);
+        if (!implementationClazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("No @Component annotation found for class: "
+                    + implementationClazz.getName());
+        }
+
+        return createInstance(implementationClazz);
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        if (!interfaceImplementations.containsKey(interfaceClazz)) {
+            throw new RuntimeException("No implementation found for "
+                    + interfaceClazz.getName());
+        }
+        return interfaceImplementations.get(interfaceClazz);
+    }
+
+    private Object createInstance(Class<?> implementationClazz) {
+        if (instances.containsKey(implementationClazz)) {
+            return instances.get(implementationClazz);
+        }
+        try {
+            Constructor<?> constructor = implementationClazz.getDeclaredConstructor();
+            if (constructor.getParameterCount() > 0) {
+                throw new RuntimeException("No default constructor found in "
+                        + implementationClazz.getName());
+            }
+
+            Object instance = constructor.newInstance();
+            instances.put(implementationClazz, instance);
+
+            for (Field field : implementationClazz.getDeclaredFields()) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    field.setAccessible(true);
+                    Object fieldInstance = getInstance(field.getType());
+                    field.set(instance, fieldInstance);
+                }
+            }
+            return instance;
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Failed to create an instance of "
+                    + implementationClazz.getName(), e);
+        }
     }
 }
--- src/main/java/mate.academy/service/impl/ProductServiceImpl.java
+++ src/main/java/mate.academy/service/impl/ProductServiceImpl.java
@@ -2,14 +2,20 @@ package mate.academy.service.impl;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import mate.academy.lib.Component;
+import mate.academy.lib.Inject;
 import mate.academy.model.Product;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 
+@Component
 public class ProductServiceImpl implements ProductService {
-    private ProductParser productParser;
+    @Inject
     private FileReaderService fileReaderService;
+
+    @Inject
+    private ProductParser productParser;
 
     @Override
     public List<Product> getAllFromFile(String filePath) {
