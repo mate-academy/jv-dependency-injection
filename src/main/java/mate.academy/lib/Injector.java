7c7f3d62b9885cedb749661fe7f358e2cc9e3e7c
Luchko Roman <romander09@gmail.com>
2025-03-22 14:07:00 +0200

    fix

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -32,13 +31,15 @@ public class Injector {
         }
 
         validateComponentAnnotation(clazz);
-        Field[] declaredFields = interfaceClazz.getDeclaredFields();
+
+        Field[] declaredFields = clazz.getDeclaredFields();
+
+        clazzImplementationInstance = createNewInstance(clazz);
+
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(Inject.class)) {
 
                 Object fieldInstance = getInstance(field.getType());
-
-                clazzImplementationInstance = createNewInstance(clazz);
 
                 try {
                     field.setAccessible(true);
@@ -64,18 +65,17 @@ public class Injector {
             Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (NoSuchMethodException | InvocationTargetException
-                 | InstantiationException | IllegalAccessException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create new instance of "
                     + clazz.getName());
         }
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
-        interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
-        interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
-        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
+        Map<Class<?>, Class<?>> interfaceImplementations = Map.of(
+                ProductParser.class, ProductParserImpl.class,
+                FileReaderService.class, FileReaderServiceImpl.class,
+                ProductService.class, ProductServiceImpl.class);
         if (interfaceClazz.isInterface()) {
             return interfaceImplementations.get(interfaceClazz);
         }
