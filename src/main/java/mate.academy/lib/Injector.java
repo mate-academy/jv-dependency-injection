8485c585c647c41e008d25a5ced17e76f9f3227e
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2025-03-27 11:54:55 +0200

    implemented AI-mentor suggestion

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -31,10 +31,10 @@ public class Injector {
                     + "Class don't have annotation @Component " + "Class: " + clazz);
         }
         Field[] declaredFields = clazz.getDeclaredFields();
+        clazzImplementationInstance = createNewInstance(clazz);
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(Inject.class)) {
                 Object fieldInstance = getInstance(field.getType());
-                clazzImplementationInstance = createNewInstance(clazz);
                 try {
                     field.setAccessible(true);
                     field.set(clazzImplementationInstance, fieldInstance);
