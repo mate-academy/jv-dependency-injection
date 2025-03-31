17d63e572a6f6ca4cbccba25a27c0380cce5b004
Alexandros <olek4004@gmail.com>
2025-03-31 19:16:26 +0200

    Injector Implementation Completed

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -14,6 +13,11 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
+    private static final Map<Class<?>, Class<?>> implementationMap = Map.of(
+            ProductParser.class, ProductParserImpl.class,
+            FileReaderService.class, FileReaderServiceImpl.class,
+            ProductService.class, ProductServiceImpl.class
+    );
     private Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
@@ -29,10 +33,13 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        if (interfaceClazz != FileReaderService.class
-                && interfaceClazz != ProductParser.class
-                && interfaceClazz != ProductService.class) {
-            throw new RuntimeException("Unsupported Class.");
+        if (!interfaceClazz.equals(FileReaderService.class)
+                && !interfaceClazz.equals(ProductParser.class)
+                && !interfaceClazz.equals(ProductService.class)) {
+            throw new RuntimeException("Unsupported Class");
+        }
+        if (interfaceClazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Class doesnt implement Component interface.");
         }
 
         Object clazzImplementationInstance = null;
@@ -46,7 +53,7 @@ public class Injector {
                 field.setAccessible(true);
                 try {
                     field.set(clazzImplementationInstance, fieldInstance);
-                } catch (IllegalAccessException e) {
+                } catch (ReflectiveOperationException e) {
                     throw new RuntimeException("Can't innitialise value.", e);
                 }
             }
@@ -67,17 +74,12 @@ public class Injector {
             Object object = constructor.newInstance();
             instances.put(clazz, object);
             return object;
-        } catch (NoSuchMethodException | InvocationTargetException
-                 | InstantiationException | IllegalAccessException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create a new instance of " + clazz.getName(), e);
         }
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> implementationMap = new HashMap<>();
-        implementationMap.put(ProductParser.class, ProductParserImpl.class);
-        implementationMap.put(FileReaderService.class, FileReaderServiceImpl.class);
-        implementationMap.put(ProductService.class, ProductServiceImpl.class);
         if (interfaceClazz.isInterface()) {
             return implementationMap.get(interfaceClazz);
         }
