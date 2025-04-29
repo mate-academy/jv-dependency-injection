2cc94177e3774d1255310a10fc7e9599e1b090f3
Sergii Nosachenko <54940595+sergii-nosachenko@users.noreply.github.com>
2025-04-29 19:33:36 +0300

    Done jv-dependency-injection

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,5 +1,7 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+
 public class Injector {
     private static final Injector injector = new Injector();
 
@@ -8,6 +10,38 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> implementation = findImplementation(interfaceClazz);
+
+        if (!implementation.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Class " + implementation.getName()
+                    + "don`t have @Component !");
+        }
+
+        try {
+            Object instance = implementation.getDeclaredConstructor().newInstance();
+
+            for (Field filed : implementation.getDeclaredFields()) {
+                if (filed.isAnnotationPresent(Inject.class)) {
+                    Object dependency = getInstance(filed.getType());
+                    filed.setAccessible(true);
+                    filed.set(instance, dependency);
+                }
+            }
+            return instance;
+        } catch (Exception e) {
+            throw new RuntimeException("Did not to create object of Class!"
+                    + implementation.getName(), e);
+        }
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        String interfaceName = interfaceClazz.getSimpleName();
+        String classImplName = "mate.academy.service.impl." + interfaceName + "Impl";
+        try {
+            return Class.forName(classImplName);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Not found implementation for interface "
+                    + interfaceClazz.getName(), e);
+        }
     }
 }
