6acb541aff1e86dd0eb515277f68ad3739ecd0da
Beliar24 <blackdid7@gmail.com>
2025-05-08 17:53:13 +0300

    reflection

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -3,15 +3,20 @@ package mate.academy;
 import java.util.List;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
+import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductService;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        // Please test your Injector here. Feel free to push this class as a part of your solution
         Injector injector = Injector.getInjector();
-        ProductService productService = null;
-        List<Product> products = productService.getAllFromFile("products.txt");
-        products.forEach(System.out::println);
+        Object actual = injector.getInstance(FileReaderService.class);
+        System.out.println(actual);
+
+//        Injector injector = Injector.getInjector();
+//        ProductService productService = null;
+//        List<Product> products = productService.getAllFromFile("products.txt");
+//        products.forEach(System.out::println);
     }
 }
